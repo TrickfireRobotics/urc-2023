4f92fbbe5aae23566cb424611f264f5c484aea77
AdamMEI <53022562+AdamMEI@users.noreply.github.com>
2022-10-07 14:44:43 -0700

    Added packages
    * Created drivebase package
    
    * Created localization package
    
    * Created teleoperation package
    
    * Created life_detection package
    
    * Created launch package
    
    * Deleted launch package

---
+++ src/life_detection/life_detection/life_detection.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from life_detection.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/teleoperation/teleoperation/teleoperation.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from teleoperation.')
+
+
+if __name__ == '__main__':
+    main()
