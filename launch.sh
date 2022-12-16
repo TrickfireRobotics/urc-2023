e11a6083f6fabcae05ed3d580f7e22d343c65bce
Adam Chhor <chhoradam@gmail.com>
2022-12-16 22:35:57 +0000

    Ros bridge now connects to websocket

--- launch.sh
+++ launch.sh
@@ -4,5 +4,6 @@ source /opt/ros/$ROS_DISTRO/setup.bash
 source /home/trickfire/urc-2023/install/setup.bash \
 
 echo "ADAMMMMMMMMMMMMMMMMMMMMMM"
-ros2 launch rosbridge_server rosbridge_websocket.launch
+ros2 launch rosbridge_server rosbridge_websocket_launch.xml
 ros2 launch viator_launch robot.launch.py
+ros2 topic pub /my_topic std_msgs/String "data: ‘Hello world!'" -1
