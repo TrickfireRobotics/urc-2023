2107854e8331e45ff838997e51771bb7e61740c2
Adam Chhor <chhoradam@gmail.com>
2022-12-16 22:13:47 +0000

    rosbridge_websocket.launch not found

--- launch.sh
+++ launch.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 source /opt/ros/$ROS_DISTRO/setup.bash
-source /home/trickfire/urc-2023/install/setup.bash
+source /home/trickfire/urc-2023/install/setup.bash \
 
+echo "ADAMMMMMMMMMMMMMMMMMMMMMM"
+ros2 launch rosbridge_server rosbridge_websocket.launch
 ros2 launch viator_launch robot.launch.py
