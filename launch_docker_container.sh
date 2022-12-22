1a8e38f4938ddd36d2f140f2cde21fcea3c2cba8
VladimirKupryukhin <vladimirkupryukhin@gmail.com>
2022-12-22 12:41:54 -0800

    Modify docker launch file, remove run_on_windows docs

--- launch_docker_container.sh
+++ launch_docker_container.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-cd .devcontainer/
-docker build -t trickfireimage ..
+docker build -t trickfireimage -f .devcontainer/Dockerfile .
 docker run --mount type=bind,source="/home/trickfire/urc-2023",target="/home/trickfire/urc-2023" -it trickfireimage
