7dc0cf7aed4ff186a526ab5858547ff366f92f04
VladimirKupryukhin <vladimirkupryukhin@gmail.com>
2022-12-22 12:57:35 -0800

    Change the source to to be #(pwd) rather than hard-coded

--- launch_docker_container.sh
+++ launch_docker_container.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker build -t trickfireimage -f .devcontainer/Dockerfile .
-docker run --mount type=bind,source="/home/trickfire/urc-2023",target="/home/trickfire/urc-2023" -it trickfireimage
+docker run --mount type=bind,source="$(pwd)",target="/home/trickfire/urc-2023" -it trickfireimage
