2da060105a71c3a286b7e02cba077d8639f619c9
Daniel K <daniel.kantor2009@gmail.com>
2023-01-13 20:26:14 +0000

    First commit, testing

---
+++ src/interface/src/my_node.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("hello world interface package\n");
+  cout << "haha interface time" << endl;
+  return 0;
+}
