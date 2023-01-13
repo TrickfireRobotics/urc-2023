4c25878ddb667b7c2821ee55a86f9553a43bdb73
Daniel K <daniel.kantor2009@gmail.com>
2023-01-13 20:17:27 +0000

    First commit, added some C++ support

--- src/interface/src/my_node.cpp
+++ src/interface/src/my_node.cpp
@@ -1,4 +1,6 @@
 #include <cstdio>
+#include <iostream>
+using namespace std;
 
 int main(int argc, char ** argv)
 {
@@ -6,5 +8,6 @@ int main(int argc, char ** argv)
   (void) argv;
 
   printf("hello world interface package\n");
+  cout << "haha new interface time" << endl;
   return 0;
 }
