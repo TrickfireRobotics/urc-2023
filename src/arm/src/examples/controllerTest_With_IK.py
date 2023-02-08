61c49205a40313a5995471734b19474c151994f2
Riley Oblander <rileyjoblander@gmail.com>
2023-02-07 16:43:10 -0800

    Moved the display function onto its own process, wow 10x faster

--- src/arm/src/examples/controllerTest_With_IK.py
+++ src/arm/src/examples/controllerTest_With_IK.py
@@ -8,6 +8,72 @@ import matplotlib.pyplot as plt
 import numpy as np
 from multiprocessing import Process
 from multiprocessing import Queue
+import time
+
+class display:
+    def __init__(self):
+        
+        self.fig, self.ax = plot_utils.init_3d_figure()
+        self.ax.legend()
+        
+        self.disp_ik_Q = Queue()
+        self.disp_target_Q = Queue()
+        
+ 
+        
+    def display_ik(self):
+        ik_chain = ikpy.chain.Chain.from_urdf_file("untitled.urdf",active_links_mask=[False, True, True, True, True, True, True])
+        ik = [0,0,0,0,0,0,0]
+        target_position = [0,0,0]
+        
+        self.fig.set_figheight(9)  
+        self.fig.set_figwidth(13)
+        ik_chain.plot(ik, self.ax, target=target_position)
+        plt.xlim(-0.5, 0.5)
+        plt.ylim(-0.5, 0.5)
+        self.ax.set_zlim(0, 0.6)
+        plt.ion()
+        plt.show()
+        
+        while (1):
+        
+            ik = self.disp_ik_Q.get()
+            target_position = self.disp_target_Q.get()
+            
+            #Start timer
+            stw = time.time()
+            stp = time.process_time()
+            
+            #0.01s
+            self.ax.clear()
+
+            #0.01s
+            ik_chain.plot(ik, self.ax, target=target_position)
+
+            #~0s
+            plt.xlim(-0.5, 0.5)
+            plt.ylim(-0.5, 0.5)
+            self.ax.set_zlim(0, 0.6)
+            self.ax.legend()
+    
+            #0.07s
+            self.fig.canvas.draw()
+      
+            #0.05
+            self.fig.canvas.flush_events()
+            
+            #End timer
+            etw = time.time()
+            etp = time.process_time()
+            
+            #Report timer
+            resw = etw - stw
+            resp = etp - stp
+            
+            if resw > 0:
+                print('Total Process time:', resw, 'seconds')
+                print('CPU Execution time:', resp, 'seconds')
+        
 
 class inverse_kinematics:
     def __init__(self):
@@ -22,9 +88,13 @@ class inverse_kinematics:
      
     def produce_ik(self):
         #initilize the process ik variabiles
-        ik_chain = ikpy.chain.Chain.from_urdf_file("arm_urdf.urdf",active_links_mask=[False, True, True, True, True, True, True])
+        ik_chain = ikpy.chain.Chain.from_urdf_file("untitled.urdf",active_links_mask=[False, True, True, True, True, True, True])
         self.ik = ik_chain.inverse_kinematics([0.38,0,0.58])
+        
         while(1):
+            #stw = time.time()
+            #stp = time.process_time()
+            
             if not self.position_Q.empty():
                 position = self.position_Q.get()
                 orientation = self.orientation_Q.get()
@@ -36,14 +106,26 @@ class inverse_kinematics:
                 # see Orientation and Position section
                 self.ik = ik_chain.inverse_kinematics(position, orientation, orientation_mode="all", initial_position=position_ik)
                 self.ikQ.put(self.ik)
-        
+            
+            
+            #etw = time.time()
+            #etp = time.process_time()
+            
+            #resw = etw - stw
+            #resp = etp - stp
+            
+            #if resw > 0:
+            #    print('Total Process time:', resw, 'seconds')
+            #    print('CPU Execution time:', resp, 'seconds')
+            
 
 
 class ControllerTest:
     def __init__(self):
     
-        self.ik_chain = ikpy.chain.Chain.from_urdf_file("arm_urdf.urdf",active_links_mask=[False, True, True, True, True, True, True])
+        self.ik_chain = ikpy.chain.Chain.from_urdf_file("untitled.urdf",active_links_mask=[False, True, True, True, True, True, True])
         self.ik_process = inverse_kinematics()
+        self.display_process = display()
         self.ik = [0,0,0,0,0,0,0]
 
         self.l_thumb_stick_pos = (0,0)
@@ -69,13 +151,11 @@ class ControllerTest:
         self.ik_target_motor_position = [0,0,0,0,0,0]
         self.motor_report_position = [0,0,0,0,0,0]
         
-        self.fig, self.ax = plot_utils.init_3d_figure()
-        self.ax.legend()
         set_deadzone(DEADZONE_TRIGGER,10) #setup a deadzone for the thumbsticks to avoid stick driftS
         
-        #Connect to two moteus drivers 
-        self.c1 = moteus.Controller(1) # 1 is extend retract antenna
-        self.c2 = moteus.Controller(2)  # 2 is rotate antenna 
+       #Connect to two moteus drivers 
+       #self.c1 = moteus.Controller(1) # 1 is extend retract antenna
+       #self.c2 = moteus.Controller(2)  # 2 is rotate antenna 
 
 
    #reference - https://github.com/Zuzu-Typ/XInput-Python
@@ -93,11 +173,11 @@ class ControllerTest:
                         # save the position of the right stick into the global variable 
                         self.r_thumb_stick_pos = (int(round(event.x,0)), int(round(event.y,0)))
                 elif event.type == EVENT_BUTTON_PRESSED:
-                    if event.button == "B":
+                    if event.button == "A":
                         self.enabled = False
-                        await self.c1.set_stop()
-                        await self.c2.set_stop()
-                    elif event.button == "A":
+                        #await self.c1.set_stoppython
+                        #await self.c2.set_stop()
+                    elif event.button == "B":
                         self.enabled = True
                     elif event.button == "LEFT_SHOULDER":
                         self.l_bumper = 1
@@ -115,16 +195,16 @@ class ControllerTest:
                         self.r_trigger_pos = event.value
             
             #Accumulate the position values from the controller
-            self.xpos += 0.01 * self.l_thumb_stick_pos[1]
-            self.ypos += 0.01 * self.l_thumb_stick_pos[0]
-            self.zpos += 0.01 * self.r_trigger_pos
-            self.zpos -= 0.01 * self.l_trigger_pos
+            self.xpos += 0.005 * self.l_thumb_stick_pos[1]
+            self.ypos += 0.005 * self.l_thumb_stick_pos[0]
+            self.zpos += 0.005 * self.r_trigger_pos
+            self.zpos -= 0.005 * self.l_trigger_pos
             
             self.target_position = [-self.xpos, self.ypos, self.zpos]
             
             #Accumulate the angle values from the controller
-            self.alpha += 0.1 * self.r_thumb_stick_pos[0]
-            self.beta += 0.1 * self.r_thumb_stick_pos[1]
+            self.alpha += 0.05 * self.r_thumb_stick_pos[0]
+            self.beta += 0.05 * self.r_thumb_stick_pos[1]
             self.gamma = 0
             
             # build the "rotational matrix" in the absolute referential see - https://en.wikipedia.org/wiki/Rotation_matrix 
@@ -143,12 +223,14 @@ class ControllerTest:
             self.target_orientation = [[aa, ab, ac],
                                       [ba, bb, bc],
                                       [ca, cb, cc]]
-            await asyncio.sleep(0.02)
+
+            await asyncio.sleep(0.01)
     
     async def consume_ik(self):
+
         while True:
 
-            print("Commanded Position: %s" % [ '%.2f' % elem for elem in self.target_position ])
+            #print("Commanded Position: %s" % [ '%.2f' % elem for elem in self.target_position ])
             #print("Commanded Orientation: %s" % [ '%.2f' % elem for elem in self.target_orientation ])
             
             self.ik_process.orientation_Q.put(self.target_orientation)
@@ -159,21 +241,27 @@ class ControllerTest:
 
             self.ik_result_position = self.ik_chain.forward_kinematics(self.ik)[:3, 3]
             
-            print("Calculated FK(cartesian) position: %s" % [ '%.2f' % elem for elem in self.ik_result_position])
+            #print("Calculated FK(cartesian) position: %s" % [ '%.2f' % elem for elem in self.ik_result_position])
             
             # motor map [hand rotation, turntable, shoulder, elbow, forarm rotation, wrist]
-            print("motor map =                   [turntable, shoulder, elbow, forarm rot, wrist, hand rot]")
-            print("Calculated IK(motor) position : %s" % [ '%.2f' % elem for elem in self.ik ][1:])
-            
-            await asyncio.sleep(0.02)
+            #print("motor map =                   [turntable, shoulder, elbow, forarm rot, wrist, hand rot]")
+            #print("Calculated IK(motor) position : %s" % [ '%.2f' % elem for elem in self.ik ][1:])
+
+            await asyncio.sleep(0.01)
         
-    async def command_motors(self):        
+    async def command_motors(self):
+        max = 0
+        first = 1    
         while True:
+        
+            stw = time.time()
+            stp = time.process_time()
+            
             # send the commanded position to the driver, with a given maximum_torque
             # query is set to True to recieve back telemetry and save that into state1 and state2
             if self.enabled:
-                state1 = await self.c1.set_position(position=self.ik[2] * 8, maximum_torque=15, query=True)
-                state2 = await self.c2.set_position(position=self.ik[1] * 8, maximum_torque=15, query=True)
+                state1 = await self.c1.set_position(position=self.ik[2], maximum_torque=15, query=True)
+                state2 = await self.c2.set_position(position=self.ik[1], maximum_torque=15, query=True)
 
                 # Read the actual position back from the recieved state variable
                 self.motor_report_position[2] = state1.values[moteus.Register.POSITION]
@@ -182,31 +270,38 @@ class ControllerTest:
 
             # Print blank line so we can separate one iteration from the
             # next
-            print("motor map =                   [turntable, shoulder, elbow, forarm rot, wrist, hand rot]")
-            print("Reported motor position : %s" % [ '%.2f' % elem for elem in self.motor_report_position ][1:])
-
             print()
+            etw = time.time()
+            etp = time.process_time()
+            
+            resw = etw - stw
+            resp = etp - stp
+            if resw > max:
+                if first == 1:
+                    first = 0
+                else:
+                    max = resw
+            if resw > 0:
+                print('Total Process time:', resw, 'seconds')
+                print('CPU Execution time:', resp, 'seconds')
+                print('MAX Process time:', max, 'seconds')
             await asyncio.sleep(0.01)
-
+    
     async def display_arm(self):
         while True:
-            #some code
-            self.ax.clear()
-            self.ik_chain.plot(self.ik, self.ax, target=self.target_position)
-            plt.xlim(-0.5, 0.5)
-            plt.ylim(-0.5, 0.5)
-            self.ax.set_zlim(0, 0.6)
-            self.ax.legend()
-            self.fig.canvas.draw()
-            self.fig.canvas.flush_events()
-            await asyncio.sleep(0.05)
+
+            
+            self.display_process.disp_ik_Q.put(self.ik)
+            self.display_process.disp_target_Q.put(self.target_position)
+
+            await asyncio.sleep(0.15)
 	
 	
     async def main(self):
         # In case the controller had faulted previously, at the start of
         # this script we send the stop command in order to clear it.
-        await self.c1.set_stop()
-        await self.c2.set_stop()
+        #await self.c1.set_stop()
+        #await self.c2.set_stop()
         
         # wait after stop command before sending the next command
         #await asyncio.sleep(0.5)
@@ -222,28 +317,22 @@ class ControllerTest:
         #pythoawait self.c2.set_stop()
 		
 		#setup the ik figure 
-        self.fig.set_figheight(9)  
-        self.fig.set_figwidth(13)  
-        self.ik_chain.plot(self.ik, self.ax, target=self.target_position)
-        plt.xlim(-0.5, 0.5)
-        plt.ylim(-0.5, 0.5)
-        self.ax.set_zlim(0, 0.6)
-        plt.ion()
-        plt.show()
+
         
         # wait before starting the main loop 
         await asyncio.sleep(0.02)
         
         ik_producer = Process(target=self.ik_process.produce_ik)
         ik_producer.start()
+        
+        ik_display = Process(target=self.display_process.display_ik)
+        ik_display.start()
 
         controller_task = asyncio.create_task(self.controller_update())
 
         ik_task = asyncio.create_task(self.consume_ik())
-   
+                    
         display_task = asyncio.create_task(self.display_arm())
-        
-        motor_task = asyncio.create_task(self.command_motors())
         
         await controller_task
                     
