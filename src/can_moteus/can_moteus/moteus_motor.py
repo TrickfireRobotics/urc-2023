201d3588a9899d8b35711ada4654ee144540b9d6
Vladimir Kupryukhin <vladimirkupryukhin@gmail.com>
2023-02-14 05:20:48 +0000

    Basic rewrite

--- src/can_moteus/can_moteus/moteus_motor.py
+++ src/can_moteus/can_moteus/moteus_motor.py
@@ -42,6 +42,22 @@ class MoteusMotor:
         #Create subscribers and publishers
         self.createPublishers(moteusPubList)
         self.createSubscribers()
+
+        #Deal with moteus controller
+        isConnected = asyncio.run(self.setupMoteusController())
+
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+
+        if (isConnected):
+            #asyncio.run(self.startLoop())
+            a = 1
+        else:
+            rosNode.get_logger().error("FAILED TO CONNECT TO CONTROLLER: NAME: " +
+                                       name + " CAN ID: " + str(self._canID))
+
+
+        
         
 
     def createModeData(self, motorMode):
@@ -102,148 +118,23 @@ class MoteusMotor:
 
 
 
+    # Create a new moteus object
+    # Start up in position mode and stop any movement of the motors
+    # If we cannot detect a motor on the CANFD/CAN bus, handle the exception
+    # Returns True if connection was successful
+    # Returns False if we had an issue connecting
+    async def setupMoteusController(self):
+
+        self.moteusController = moteus.Controller(self._canID)
+
+        try:
+            await self.moteusController.set_stop()
+        except RuntimeError as error:
+            self._rosNode.get_logger().error(error.__str__())
+            return False
+
+        return True
+
+    
 
 
-
-
-
-
-
-
-
-    # _canID = None  # int
-    # _name = None  # string
-    # _moteusRegToDataHashmap = None  # Moteus.Register -> int32 (for now)
-    # _moteusRegToPubObjHashmap = None  # Moteus.Register -> ROS Publisher object
-    # rosNode = None  # The ros_moteus_bridge.py
-    # moteusController = None  # moteus.Controller
-    # _motorMode = None  # The mode in which the motor is in
-
-    # def __init__(self, mode, canID, name, moteusPubList, moteusSubList, rosNode):
-    #     rosNode.get_logger().info("Creating motor with name: " + name)
-    #     self._canID = canID
-    #     self._name = name
-    #     self._moteusRegToDataHashmap = dict()
-    #     self._moteusRegToPubObjHashmap = dict()
-    #     self.rosNode = rosNode
-    #     self._motorMode = mode
-
-    #     # Setup subscriber stuff
-    #     self.createMoteusToDataHashmap(moteusSubList)
-    #     self.createSubscribers(moteusSubList, rosNode)
-
-    #     # Setup publisher stuff
-    #     self.createPublishers(moteusPubList)
-
-    #     # If for some reason it could not find the motor, do not start anything
-    #     isConnected = asyncio.run(self.setupMoteusController())
-    #     if (isConnected):
-    #         asyncio.run(self.startLoop())
-    #     else:
-    #         rosNode.get_logger().error("FAILED TO CONNECT TO CONTROLLER: NAME: " +
-    #                                    name + " CAN ID: " + str(self._canID))
-
-    # async def startLoop(self):
-    #     while True:
-    #         returnedData = ""
-
-    #         if (self._motorMode == self.Mode.POSITION):
-    #             returnedData = await self.moteusController.set_position(
-    #                 position=self._moteusRegToDataHashmap[moteus.Register.POSITION], maximum_torque=15, query=True)
-
-    #         if (self._motorMode == self.Mode.CURRENT):
-    #             returnedData = await self.moteusController.set_current(
-    #                 d_A=self._moteusRegToDataHashmap[moteus.Register.D_CURRENT],
-    #                 q_A=self._moteusRegToDataHashmap[moteus.Register.Q_CURRENT],
-    #                 query=True
-    #             )
-
-    #         # What is VFOC? Do we need it?
-    #         # if(self.motorMode == self.Mode.VFOC)
-
-    #         # Publish data to ROS
-    #         self.publishDataToRos(returnedData)
-    #         asyncio.sleep(0.02)
-
-    # # Goes through all of the publishers and sends the correct data from the
-    # # data we got from the motors
-
-    # async def publishDataToRos(self, moteusResult):
-    #     for register in self._moteusRegToPubObjHashmap:
-    #         publisher = self._moteusRegToPubObjHashmap[register]
-    #         data = moteusResult.values(register)
-
-    #         publisher.publish(data)
-
-    # # For each register/data that we want to publish to ROS,
-    # # create a specific publisher topic for it.
-    # # The format of the publisher is:
-    # # <name of the motor>_<the data being sent via moteus.register>_from_can
-    # def createPublishers(self, moteusPubList):
-    #     for register in moteusPubList:
-    #         topicName = self._name + "_" + \
-    #             str(register).replace("Register.", "").lower() + "_from_can"
-    #         publisher = self.rosNode.create_publisher(
-    #             std_msgs.msg.Int32, topicName, 10)
-
-    #         self._moteusRegToPubObjHashmap[register] = publisher
-
-    # # Create a new moteus object
-    # # Start up in position mode and stop any movement of the motors
-    # # If we cannot detect a motor on the CANFD/CAN bus, handle the exception
-    # # Returns True if connection was successful
-    # # Returns False if we had an issue connecting
-    # async def setupMoteusController(self):
-
-    #     self.moteusController = moteus.Controller(self._canID)
-
-    #     try:
-    #         await self.moteusController.set_stop()
-    #         # await self.moteusController.set_position(position=math.nan, query=False)
-    #     except RuntimeError as error:
-    #         self.rosNode.get_logger().error(error.__str__())
-    #         return False
-
-    #     return True
-
-    # # Creates a map between the moteus registers and their respective data
-    # def createMoteusToDataHashmap(self, moteusSubList):
-    #     for moteusRegister in moteusSubList:
-    #         self._moteusRegToDataHashmap[moteusRegister] = 0
-
-    # # Create a subscriber for each of the registers to listen to
-
-    # # Go through each register that we want to subscribe to, position, current, etc
-    # # and create a subscriber for it. Each subscriber will then modify the
-    # # _moteusRegToDataHashmap with the the data recieved
-    # # The format of the subscriber is:
-    # # <name of the motor>_<the data being recieved via moteus.register>_from_robot_interface
-    # def createSubscribers(self, moteusSubList, rosNode):
-    #     for register in moteusSubList:
-    #         topicName = self._name + "_" + \
-    #             str(register).replace("Register.", "").lower() + \
-    #             "_from_robot_interface"
-    #         rosNode.subscription = rosNode.create_subscription(
-    #             std_msgs.msg.Int32,
-    #             topicName,
-    #             (lambda self, moteus_motor=self,
-    #                 reg=register: moteus_motor.updateDataHashmap(reg, self)),
-    #             10
-    #         )
-
-    #         rosNode.get_logger().info(str(topicName))
-
-    # # Update the data when we got it from the subscriber
-    # def updateDataHashmap(self, register, data):
-    #     self.rosNode.get_logger().info("Change data for register: " + str(register))
-    #     self._moteusRegToDataHashmap[register] = data
-
-    # # This describes which mode the motor will run in
-    # # Technically, the moteus code will change the mode for us
-    # # when we call set_position or set_current. But to prevent
-    # # accidental use of the incorrect moteus controller mode,
-    # # use this so that the correct functions are called.
-    # class Mode:
-    #     POSITION = 0
-    #     CURRENT = 1
-    #     VFOC = 2
