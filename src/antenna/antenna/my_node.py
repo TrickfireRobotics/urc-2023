0afb3d00d2dc46ba294606a8b517c85a105e8b2b
Dong <dongn12@uw.edu>
2023-02-17 23:44:25 +0000

    turn Antenna when the signal is not strong

--- src/antenna/antenna/my_node.py
+++ src/antenna/antenna/my_node.py
@@ -1,12 +1,9 @@
 import rssi
+import time
+import robotInterface
 
 
-def isSignalGood(stringSignal):
-    signal = int(stringSignal)
-    return signal > -100
-
-
-def checkSignal():
+def isSignalGood():
     interface = 'wlp1s0'
     rssi_scanner = rssi.RSSI_Scan(interface)
 
@@ -15,13 +12,36 @@ def checkSignal():
     # sudo argument automatixally gets set for 'false', if the 'true' is not set manually.
     # python file will have to be run with sudo privileges.
     ap_info = rssi_scanner.getAPinfo(networks=ssids, sudo=True)
+    signal = int(ap_info[0])
+
     # check if signal is good
-    return isSignalGood(ap_info[0])
+    return signal > -100
+
+
+def turnAntenna():
+    turnDegree = 0
+    # turn right if the signal bad and until get to 180 degree to the right
+    while (isSignalGood() != True & turnDegree < 90):
+        turnDegree += 10
+        robotInterface.antennaTurnRight(turnDegree)
+
+    # if after turn all to the right but still don't have the good signal turn left
+    if (isSignalGood() != True):
+        turnDegree = 0
+        # turn left until the signal good or turn to 180 degree to the left
+        # turnDegree less than 360 degree because the first 180 degree
+        # is turn back to the begin after turn right
+        while (isSignalGood() != True & turnDegree < 180):
+            turnDegree += 10
+            robotInterface.antennaTurnLeft(turnDegree)
 
 
 def main():
     while (true):
-        checkSignal()
+        if (isSignalGood() != True):
+            print("not good")
+        turnAntenna()
+        time.sleep(0.2)
     # check the signal if it good
     # if it's not good move the arms
 
---
+++ src/antenna/antenna/robotInterface.py
@@ -0,0 +1,145 @@
+# General interface for robot funcitonality
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+
+class RobotInterface(Node):
+    #
+    # Constructors/Destructors
+    #
+
+    def __init__(self):
+        super().__init__('robot_interface_publisher')
+        self.publisher_ = self.create_publisher(float, 'topic', 10)
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
+    def __del__(self):
+        print()
+
+    # TESTING
+    #
+    # def timer_callback(self):
+    #     msg = String()
+    #     msg.data = 'Hello World: %d' % self.i
+    #     self.publisher_.publish(msg)
+    #     self.get_logger().info('Publishing: "%s"' % msg.data)
+    #     self.i += 1
+
+    # RobotInterface(const RobotInterface&) = delete;
+    # RobotInterface& operator=(const RobotInterface&) = delete;
+    # RobotInterface(RobotInterface&&) = delete;
+    # RobotInterface& operator=(RobotInterface&&) = delete;
+
+    #
+    # Movement
+    #
+
+    # General movement including all wheels
+
+    def moveForward(self, amount):
+        msg = float()
+        msg.data = amount
+        self.publisher_.publish(msg)
+        self.get_logger().info('moveForward publishing: "%f"' % msg.data)
+
+    def moveBackward(self, amount):
+        print()
+
+    def turnLeft(self, amount):
+        print()
+
+    def turnRight(self, amount):
+        print()
+
+    # Left front wheel
+    def leftFrontWheelForward(self, amount):
+        print()
+
+    def leftFrontWheelBackward(self, amount):
+        print()
+
+    # Right front wheel
+    def rightFrontWheelForward(self, amount):
+        print()
+
+    def rightFrontWheelBackward(self, amount):
+        print()
+
+    # Left middle wheel
+    def leftMiddleWheelForward(self, amount):
+        print()
+
+    def leftMiddleWheelBackward(self, amount):
+        print()
+
+    # Right middle wheel
+    def rightMiddleWheelForward(self, amount):
+        print()
+
+    def rightMiddleWheelBackward(self, amount):
+        print()
+
+    # Left back wheel
+    def leftBackWheelForward(self, amount):
+        print()
+
+    def leftBackWheelBackward(self, amount):
+        print()
+
+    # Right back wheel
+    def rightBackWheelForward(self, amount):
+        print()
+
+    def rightBackWheelBackward(self, amount):
+        print()
+
+    #
+    # Arm movement/rotation
+    #
+
+    # Base turntable
+    def baseRotationLeft(self, amount):
+        print()
+
+    def baseRotationRight(self, amount):
+        print()
+
+    # Arm 1 is closest to base and arm 3 is furthest from base
+
+    # Arm 1 movement
+    def arm1Extend(self, amount):
+        print()
+
+    def arm1Retract(self, amount):
+        print()
+
+    # Arm 2 movement
+    def arm2Extend(self, amount):
+        print()
+
+    def arm2Retract(self, amount):
+        print()
+
+    # Arm 3 movement
+    def arm3Extend(self, amount):
+        print()
+
+    def arm3Retract(self, amount):
+        print()
+
+    # Atenna movement
+    def antennaExtend(self):
+        print()
+
+    def antennaRetract(self):
+        print()
+
+    def antennaTurnLeft(self, amount):
+        print()
+
+    def antennaTurnRight(self, amount):
+        print()
