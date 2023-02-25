cc9956935564d4028896eff6075142439138718f
Adam Chhor <chhoradam@gmail.com>
2023-02-25 11:41:30 +0000

    cleaned up code and UI and fixed messageType bug

--- src/mission_control_py/webpage/script.js
+++ src/mission_control_py/webpage/script.js
@@ -1,55 +1,77 @@
-const testForLatency = (e) => {
-  const currTime = Date.now();
-  
-}
+// Create ros object to communicate over your Rosbridge connection
+const ros = new ROSLIB.Ros({ url: 'ws://localhost:9090' });
 
-
-
-// //controller stuff
-window.addEventListener("gamepadconnected", (e) => {
-  console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
-    e.gamepad.index, e.gamepad.id,
-    e.gamepad.buttons.length, e.gamepad.axes.length);
+// When the Rosbridge server connects, fill the span with id “status" with “successful"
+ros.on('connection', () => {
+  document.getElementById("status").innerHTML = "successful";
 });
-// console.log("HELLO WORLD");
 
+// When the Rosbridge server experiences an error, fill the “status" span with the returned error
+ros.on('error', (error) => {
+  document.getElementById("status").innerHTML = `errored out (${error})`;
+});
+
+// When the Rosbridge server shuts down, fill the “status" span with “closed"
+ros.on('close', () => {
+  document.getElementById("status").innerHTML = "closed";
+});
 
 //listens to mission_control_py
-
-const my_topic_listener = new ROSLIB.Topic({
+const latency_listener = new ROSLIB.Topic({
   ros,
   name : "/latency",
   messageType : "std_msgs/String"
 });
-const move_right_drivebase_side = new ROSLIB.Topic({
-  ros,
-  name : "/move_right_drivebase_side",
-  messageType : "std_msgs/Float32MultiArray"
-});
-setInterval(() => {
-  const myGamepad = navigator.getGamepads()[0]; // use the first gamepad
-  let move_right_drivebase_side_data = myGamepad.axes[0];
-  console.log(`Left stick at (${myGamepad.axes[0]}, ${myGamepad.axes[1]})` );
-  console.log(`Right stick at (${myGamepad.axes[2]}, ${myGamepad.axes[3]})` );
-  move_right_drivebase_side.publish("" + move_right_drivebase_side_data);
-}, 2000) // print axes 10 times per second
-
 // When we receive a message on /my_topic, add its data as a list item to the “messages" ul
-my_topic_listener.subscribe((message) => {
-  // let rosTime = message.data.
+latency_listener.subscribe((message) => {
   let currTime = Date.now();
-  // console.log(currTime);
   let start = message.data.indexOf("=")
   let end = message.data.indexOf(",")
   let rosTime = message.data.substring(start +1,end)
   let decimalPlaces = 1;
   rosTime = rosTime.substring(0,currTime.toString().length);
-  // rosTime /= 10;
-  // rosTime /= (Math.pow(10,decimalPlaces));
-  console.log(`${currTime-rosTime}ms`);
+  let latency =currTime-rosTime;
+  console.log(`${latency}ms`);
   //trimming string
   
-  // const ul = document. getElementById("messages");
+  document.getElementById("latency").innerHTML = `${latency}ms` ;
+
   // const newMessage = document. createElement("li");
   // newMessage. appendChild(document. createTextNode(message.data));
 });
+//controller stuff
+window.addEventListener("gamepadconnected", (e) => {
+  console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
+    e.gamepad.index, e.gamepad.id,
+    e.gamepad.buttons.length, e.gamepad.axes.length);
+});
+
+const move_left_drivebase_side = new ROSLIB.Topic({
+  label: 'float[2] containing left controler stick',
+  ros : ros,
+  name : "/move_right_drivebase_side",
+  messageType : "std_msgs/msg/Float32MultiArray"
+});
+const move_right_drivebase_side = new ROSLIB.Topic({
+  label: 'float[2] containing right controler stick',
+  ros : ros,
+  name : "/move_right_drivebase_side",
+  messageType : "std_msgs/msg/Float32MultiArray"
+});
+setInterval(() => {
+  const myGamepad = navigator.getGamepads()[0]; // use the first gamepad
+  console.log(`Left stick at (${myGamepad.axes[0]}, ${myGamepad.axes[1]})` );
+  console.log(`Right stick at (${myGamepad.axes[2]}, ${myGamepad.axes[3]})` );
+  let move_left_drivebase_side_data = [myGamepad.axes[0],myGamepad.axes[1]]
+  let move_right_drivebase_side_data = [myGamepad.axes[1],myGamepad.axes[2]]
+  document.getElementById("left-stick").innerHTML = `[${move_left_drivebase_side_data}]`;
+  document.getElementById("right-stick").innerHTML = `[${move_right_drivebase_side_data}]` ;
+  
+  let move_left_drivebase_side_message = new ROSLIB.Message({
+    data: move_left_drivebase_side_data});
+  let move_right_drivebase_side_message = new ROSLIB.Message({
+    data: move_right_drivebase_side_data});
+  move_left_drivebase_side.publish(move_left_drivebase_side_message);
+  move_right_drivebase_side.publish(move_right_drivebase_side_message);
+
+}, 500) // 2 times per second
