0db00f2e867fd4d70283fae8021af0c854a61714
Adam Chhor <chhoradam@gmail.com>
2023-02-25 10:16:24 +0000

    added /move_right_drivebase_side publisher in webpage

--- launch.sh
+++ launch.sh
@@ -3,6 +3,8 @@
 source /opt/ros/$ROS_DISTRO/setup.bash
 source /home/trickfire/urc-2023/install/setup.bash \
 
-ros2 topic list
-# ros2 run mission_control_py talker
-ros2 launch viator_launch robot.launch.py rosbridge_server rosbridge_websocket_launch.xml
+# ros2 launch viator_launch robot.launch.py 
+ros2 run mission_control_py talker
+ros2 launch rosbridge_server rosbridge_websocket_launch.xml
+
+# ros2 topic list
--- src/mission_control_py/webpage/script.js
+++ src/mission_control_py/webpage/script.js
@@ -6,18 +6,12 @@ const testForLatency = (e) => {
 
 
 // //controller stuff
-// window.addEventListener("gamepadconnected", (e) => {
-//   console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
-//     e.gamepad.index, e.gamepad.id,
-//     e.gamepad.buttons.length, e.gamepad.axes.length);
-// });
+window.addEventListener("gamepadconnected", (e) => {
+  console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
+    e.gamepad.index, e.gamepad.id,
+    e.gamepad.buttons.length, e.gamepad.axes.length);
+});
 // console.log("HELLO WORLD");
-// setInterval(() => {
-// 	const myGamepad = navigator.getGamepads()[0]; // use the first gamepad
-// 	console.log(`Left stick at (${myGamepad.axes[0]}, ${myGamepad.axes[1]})` );
-// 	console.log(`Right stick at (${myGamepad.axes[2]}, ${myGamepad.axes[3]})` );
-// 	console.log(myGamepad.buttons);
-// }, 2000) // print axes 10 times per second
 
 
 //listens to mission_control_py
@@ -27,6 +21,18 @@ const my_topic_listener = new ROSLIB.Topic({
   name : "/latency",
   messageType : "std_msgs/String"
 });
+const move_right_drivebase_side = new ROSLIB.Topic({
+  ros,
+  name : "/move_right_drivebase_side",
+  messageType : "std_msgs/Float32MultiArray"
+});
+setInterval(() => {
+  const myGamepad = navigator.getGamepads()[0]; // use the first gamepad
+  let move_right_drivebase_side_data = myGamepad.axes[0];
+  console.log(`Left stick at (${myGamepad.axes[0]}, ${myGamepad.axes[1]})` );
+  console.log(`Right stick at (${myGamepad.axes[2]}, ${myGamepad.axes[3]})` );
+  move_right_drivebase_side.publish("" + move_right_drivebase_side_data);
+}, 2000) // print axes 10 times per second
 
 // When we receive a message on /my_topic, add its data as a list item to the “messages" ul
 my_topic_listener.subscribe((message) => {
@@ -38,7 +44,6 @@ my_topic_listener.subscribe((message) => {
   let rosTime = message.data.substring(start +1,end)
   let decimalPlaces = 1;
   rosTime = rosTime.substring(0,currTime.toString().length);
-
   // rosTime /= 10;
   // rosTime /= (Math.pow(10,decimalPlaces));
   console.log(`${currTime-rosTime}ms`);
