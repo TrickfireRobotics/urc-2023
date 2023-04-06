41f5d8c71e12ae378e8598b90d6795c3c578e3b0
adamseth2 <chhoradam@gmail.com>
2023-04-06 16:45:37 +0000

    added rossuit submodule, websocket runs from viator launcher

--- launch.sh
+++ launch.sh
@@ -3,8 +3,8 @@
 source /opt/ros/$ROS_DISTRO/setup.bash
 source /home/trickfire/urc-2023/install/setup.bash \
 
-# ros2 launch viator_launch robot.launch.py 
+ros2 launch viator_launch robot.launch.py 
 # ros2 run mission_control mission_control
-ros2 launch rosbridge_server rosbridge_websocket_launch.xml
+# ros2 launch rosbridge_server rosbridge_websocket_launch.xml
 
 # ros2 topic list
--- src/viator_launch/launch/robot.launch.py
+++ src/viator_launch/launch/robot.launch.py
@@ -1,7 +1,11 @@
 import launch
 from launch_ros.actions import ComposableNodeContainer
+from launch.actions import IncludeLaunchDescription
 from launch_ros.descriptions import ComposableNode
 from launch_ros.actions import Node
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
+import os
+from ament_index_python import get_package_share_directory
 
 hello_node = ComposableNode(
     package='hello_world',
@@ -32,7 +36,12 @@ robot_container = ComposableNodeContainer(
 
 
 def generate_launch_description():
-    return launch.LaunchDescription([
-        robot_container,
-        mission_control_node
-    ])
+    ld = launch.LaunchDescription()
+    rosbridge_launch = IncludeLaunchDescription(
+        XMLLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('rosbridge_server'), 
+                         'launch/rosbridge_websocket_launch.xml')))
+    ld.add_action(robot_container)
+    ld.add_action(mission_control_node)
+    ld.add_action(rosbridge_launch)
+    return ld
