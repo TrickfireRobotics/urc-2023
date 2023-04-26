18b1fc16f3388d954d7f0f7464c6e9d2496a2c46
mendomic <mendomic@uw.edu>
2023-04-26 13:42:44 -0700

    Added Arm publisher and renamed other publishers

--- src/interface/src/robot_interface.cpp
+++ src/interface/src/robot_interface.cpp
@@ -20,30 +20,42 @@ using namespace std;
 RobotInterface::RobotInterface(Node rosNode) {
     self.selfNode = rosNode;
     cout << "GOT TO INIT" << endl;
-    publisher_ = this->create_publisher<float>("leftFrontWheel_Velocity_from_interface", 10);
-    robotPublishers['leftFrontWheel'] = publisher_;
-    publisher_ = this->create_publisher<float>("rightFrontWheel_Velocity_from_interface", 10);
-    robotPublishers['rightFrontWheel'] = publisher_;
-    publisher_ = this->create_publisher<float>("leftMiddleWheel_Velocity_from_interface", 10);
-    robotPublishers['leftMiddleWheel'] = publisher_;
-    publisher_ = this->create_publisher<float>("rightMiddleWheel_Velocity_from_interface", 10);
-    robotPublishers['rightMiddleWheel'] = publisher_;
-    publisher_ = this->create_publisher<float>("leftBackWheel_Velocity_from_interface", 10);
-    robotPublishers['leftBackWheel'] = publisher_;
-    publisher_ = this->create_publisher<float>("rightBackWheel_Velocity_from_interface", 10);
-    robotPublishers['rightBackWheel'] = publisher_;
-    publisher_ = this->create_publisher<float>("baseRotation_Position_from_interface", 10);
-    robotPublishers['baseRotation'] = publisher_;
-    publisher_ = this->create_publisher<float>("arm1_Position_from_interface", 10);
-    robotPublishers['arm1'] = publisher_;
-    publisher_ = this->create_publisher<float>("arm2_Position_from_interface", 10);
-    robotPublishers['arm2'] = publisher_;
-    publisher_ = this->create_publisher<float>("arm3_Position_from_interface", 10);
-    robotPublishers['arm3'] = publisher_;
-    publisher_ = this->create_publisher<float>("antenna_Position_from_interface", 10);
-    robotPublishers['antenna'] = publisher_;
-    publisher_ = this->create_publisher<float>("antennaTurntable_Position_from_interface", 10);
-    robotPublishers['antennaTurntable'] = publisher_;
+    
+    // Drive base
+    publisher_ = this->create_publisher<float>("front_left_wheel_motor_velocity_from_interface", 10);
+    robotPublishers['front_left_wheel_motor'] = publisher_;
+    publisher_ = this->create_publisher<float>("front_right_wheel_motor_velocity_from_interface", 10);
+    robotPublishers['front_right_wheel_motor'] = publisher_;
+    publisher_ = this->create_publisher<float>("mid_left_wheel_motor_velocity_from_interface", 10);
+    robotPublishers['mid_left_wheel_motor'] = publisher_;
+    publisher_ = this->create_publisher<float>("mid_right_wheel_motor_velocity_from_interface", 10);
+    robotPublishers['mid_right_wheel_motor'] = publisher_;
+    publisher_ = this->create_publisher<float>("rear_left_wheel_motor_velocity_from_interface", 10);
+    robotPublishers['rear_left_wheel_motor'] = publisher_;
+    publisher_ = this->create_publisher<float>("rear_right_wheel_motor_velocity_from_interface", 10);
+    robotPublishers['rear_right_wheel_motor'] = publisher_;
+
+    // Arm
+    publisher_ = this->create_publisher<float>("arm_turntable_motor_position_from_interface", 10);
+    robotPublishers['arm_turntable_motor'] = publisher_;
+    publisher_ = this->create_publisher<float>("arm_shoulder_motor_position_from_interface", 10);
+    robotPublishers['arm_shoulder_motor'] = publisher_;
+    publisher_ = this->create_publisher<float>("arm_elbow_motor_position_from_interface", 10);
+    robotPublishers['arm_elbow_motor'] = publisher_;
+    publisher_ = this->create_publisher<float>("arm_forearm_motor_position_from_interface", 10);
+    robotPublishers['arm_forearm_motor'] = publisher_;
+    publisher_ = this->create_publisher<float>("arm_wrist_motor_position_from_interface", 10);
+    robotPublishers['arm_wrist_motor'] = publisher_;
+    publisher_ = this->create_publisher<float>("arm_hand_motor_position_from_interface", 10);
+    robotPublishers['arm_hand_motor'] = publisher_;
+    publisher_ = this->create_publisher<float>("arm_fingers_motor_position_from_interface", 10);
+    robotPublishers['arm_fingers_motor'] = publisher_;
+
+    // Antenna
+    publisher_ = this->create_publisher<float>("antenna_motor_position_from_interface", 10);
+    robotPublishers['antenna_motor'] = publisher_;
+    publisher_ = this->create_publisher<float>("antenna_turntable_motor_position_from_interface", 10);
+    robotPublishers['antenna_turntable_motor'] = publisher_;
 }
 
 RobotInterface::~RobotInterface() {
@@ -78,200 +90,245 @@ void RobotInterface::turnRight(const double amount) {
 }
 
 // Left front wheel
-void RobotInterface::leftFrontWheel(const double amount) {
-    cout << "Testing leftFrontWheel" << endl;
+void RobotInterface::front_left_wheel_motor(const double amount) {
+    cout << "Testing front_left_wheel_motor" << endl;
 
     //Converting from input "meters per second" to output "revolutions per second"
     double revolutionsOutput = 1.76838724851 * amount;
 
-    publisher_ = robotPublishers['leftFrontWheel']
+    publisher_ = robotPublishers['front_left_wheel_motor']
     auto message = std_msgs::msg::String();
     message.data = std::to_string(revolutionsOutput);
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
-    cout << "Publishing leftFrontWheel: " + message.data << endl;
+    cout << "Publishing front_left_wheel_motor: " + message.data << endl;
 }
 
 // Right front wheel
-void RobotInterface::rightFrontWheel(const double amount) {
-    cout << "Testing rightFrontWheel" << endl;
+void RobotInterface::front_right_wheel_motor(const double amount) {
+    cout << "Testing front_right_wheel_motor" << endl;
 
     //Converting from input "meters per second" to output "revolutions per second"
     double revolutionsOutput = 1.76838724851 * amount;
 
-    publisher_ = robotPublishers['rightFrontWheel'];
+    publisher_ = robotPublishers['front_right_wheel_motor'];
     auto message = std_msgs::msg::String();
     message.data = std::to_string(revolutionsOutput);
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
-    cout << "Publishing rightFrontWheel: " + message.data << endl;
+    cout << "Publishing front_right_wheel_motor: " + message.data << endl;
 }
 
 // Left middle wheel
-void RobotInterface::leftMiddleWheel(const double amount) {
-    cout << "Testing leftMiddleWheel" << endl;
+void RobotInterface::mid_left_wheel_motor(const double amount) {
+    cout << "Testing mid_left_wheel_motor" << endl;
 
     //Converting from input "meters per second" to output "revolutions per second"
     double revolutionsOutput = 1.76838724851 * amount;
 
-    publisher_ = robotPublishers['leftMiddleWheel'];
+    publisher_ = robotPublishers['mid_left_wheel_motor'];
     auto message = std_msgs::msg::String();
     message.data = std::to_string(revolutionsOutput);
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
-    cout << "Publishing leftMiddleWheel: " + message.data << endl;
+    cout << "Publishing mid_left_wheel_motor: " + message.data << endl;
 }
 
 // Right middle wheel
-void RobotInterface::rightMiddleWheel(const double amount) {
-    cout << "Testing rightMiddleWheel" << endl;
+void RobotInterface::mid_right_wheel_motor(const double amount) {
+    cout << "Testing mid_right_wheel_motor" << endl;
 
     //Converting from input "meters per second" to output "revolutions per second"
     double revolutionsOutput = 1.76838724851 * amount;
 
-    publisher_ = robotPublishers['rightMiddleWheel'];
+    publisher_ = robotPublishers['mid_right_wheel_motor'];
     auto message = std_msgs::msg::String();
     message.data = std::to_string(revolutionsOutput);
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
-    cout << "Publishing rightMiddleWheel: " + message.data << endl;
+    cout << "Publishing mid_right_wheel_motor: " + message.data << endl;
 }
 
 // Left back wheel
-void RobotInterface::leftBackWheel(const double amount) {
-    cout << "Testing leftBackWheel" << endl;
+void RobotInterface::rear_left_wheel_motor(const double amount) {
+    cout << "Testing rear_left_wheel_motor" << endl;
 
     //Converting from input "meters per second" to output "revolutions per second"
     double revolutionsOutput = 1.76838724851 * amount;
 
-    publisher_ = robotPublishers['leftBackWheel'];
+    publisher_ = robotPublishers['rear_left_wheel_motor'];
     auto message = std_msgs::msg::String();
     message.data = std::to_string(revolutionsOutput);
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
-    cout << "Publishing leftBackWheel: " + message.data << endl;
+    cout << "Publishing rear_left_wheel_motor: " + message.data << endl;
 }
 
 // Right back wheel
-void RobotInterface::rightBackWheel(const double amount) {
-    cout << "Testing rightBackWheel" << endl;
+void RobotInterface::rear_right_wheel_motor(const double amount) {
+    cout << "Testing rear_right_wheel_motor" << endl;
 
     //Converting from input "meters per second" to output "revolutions per second"
     double revolutionsOutput = 1.76838724851 * amount;
 
-    publisher_ = robotPublishers['rightBackWheel'];
+    publisher_ = robotPublishers['rear_right_wheel_motor'];
     auto message = std_msgs::msg::String();
     message.data = std::to_string(revolutionsOutput);
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
-    cout << "Publishing rightBackWheel: " + message.data << endl;
+    cout << "Publishing rear_right_wheel_motor: " + message.data << endl;
 }
 
 /*
  * Arm movement/rotation
  */
 
-// Base turntable
-void RobotInterface::baseRotation(const double amount) {
-    cout << "Testing baseRotation" << endl;
+// Arm turntable
+void RobotInterface::arm_turntable_motor(const double amount) {
+    cout << "Testing arm_turntable_motor" << endl;
 
     //Converting from input "degrees" to output "revolutions"
     double revolutionsOutput = amount / 360;
 
-    publisher_ = robotPublishers['baseRotation'];
+    publisher_ = robotPublishers['arm_turntable_motor'];
     auto message = std_msgs::msg::String();
     message.data = std::to_string(revolutionsOutput);
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
-    cout << "Publishing baseRotation: " + message.data << endl;
+    cout << "Publishing arm_turntable_motor: " + message.data << endl;
 }
 
 // Arm 1 is closest to base and arm 3 is furthest from base
 
 // Arm 1 movement
-void RobotInterface::arm1(const double amount) {
-    cout << "Testing arm1" << endl;
+void RobotInterface::arm_shoulder_motor(const double amount) {
+    cout << "Testing arm_shoulder_motor" << endl;
 
     //Converting from input "degrees" to output "revolutions"
     double revolutionsOutput = amount / 360;
 
-    publisher_ = robotPublishers['arm1'];
+    publisher_ = robotPublishers['arm_shoulder_motor'];
     auto message = std_msgs::msg::String();
     message.data = std::to_string(revolutionsOutput);
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
-    cout << "Publishing arm1: " + message.data << endl;
+    cout << "Publishing arm_shoulder_motor: " + message.data << endl;
 }
 
 // Arm 2 movement
-void RobotInterface::arm2(const double amount) {
-    cout << "Testing arm2" << endl;
+void RobotInterface::arm_elbow_motor(const double amount) {
+    cout << "Testing arm_elbow_motor" << endl;
 
     //Converting from input "degrees" to output "revolutions"
     double revolutionsOutput = amount / 360;
 
-    publisher_ = robotPublishers['arm2'];
+    publisher_ = robotPublishers['arm_elbow_motor'];
     auto message = std_msgs::msg::String();
     message.data = std::to_string(revolutionsOutput);
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
-    cout << "Publishing arm2: " + message.data << endl;
+    cout << "Publishing arm_elbow_motor: " + message.data << endl;
 }
 
 // Arm 3 movement
-void RobotInterface::arm3(const double amount) {
-    cout << "Testing arm3" << endl;
+void RobotInterface::arm_forearm_motor(const double amount) {
+    cout << "Testing arm_forearm_motor" << endl;
 
     //Converting from input "degrees" to output "revolutions"
     double revolutionsOutput = amount / 360;
 
-    publisher_ = robotPublishers['arm3'];
+    publisher_ = robotPublishers['arm_forearm_motor'];
     auto message = std_msgs::msg::String();
     message.data = std::to_string(revolutionsOutput);
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
-    cout << "Publishing arm3: " + message.data << endl;
+    cout << "Publishing arm_forearm_motor: " + message.data << endl;
 }
 
-// Antenna movement
-void RobotInterface::antennaExtend() {
-    cout << "Testing antenna" << endl;
+// Arm wrist movement
+void RobotInterface::arm_wrist_motor(const double amount) {
+    cout << "Testing arm_wrist_motor" << endl;
+
+    //Converting from input "degrees" to output "revolutions"
+    double revolutionsOutput = amount / 360;
+
+    publisher_ = robotPublishers['arm_wrist_motor'];
+    auto message = std_msgs::msg::String();
+    message.data = std::to_string(revolutionsOutput);
+    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+    publisher_->publish(message);
+    cout << "Publishing arm_wrist_motor: " + message.data << endl;
+}
+
+// Arm hand movement
+void RobotInterface::arm_hand_motor(const double amount) {
+    cout << "Testing arm_hand_motor" << endl;
+
+    //Converting from input "degrees" to output "revolutions"
+    double revolutionsOutput = amount / 360;
+
+    publisher_ = robotPublishers['arm_hand_motor'];
+    auto message = std_msgs::msg::String();
+    message.data = std::to_string(revolutionsOutput);
+    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+    publisher_->publish(message);
+    cout << "Publishing arm_hand_motor: " + message.data << endl;
+}
+
+// Arm fingers movement
+void RobotInterface::arm_fingers_motor(const double amount) {
+    cout << "Testing arm_fingers_motor" << endl;
+
+    //Converting from input "degrees" to output "revolutions"
+    double revolutionsOutput = amount / 360;
+
+    publisher_ = robotPublishers['arm_fingers_motor'];
+    auto message = std_msgs::msg::String();
+    message.data = std::to_string(revolutionsOutput);
+    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+    publisher_->publish(message);
+    cout << "Publishing arm_fingers_motor: " + message.data << endl;
+}
+
+// antenna_motor movement
+void RobotInterface::antenna_motor_extend() {
+    cout << "Testing antenna_motor" << endl;
 
     // Output is in "revolutions"
     doublerevolutionsOutput = 1; // we don't know the actual position it should be. this is a placeholder
 
-    publisher_ = robotPublishers['antenna'];
+    publisher_ = robotPublishers['antenna_motor'];
     auto message = std_msgs::msg::String();
     message.data = std::to_string(revolutionsOutput);
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
-    cout << "Publishing antenna: " + message.data << endl;
+    cout << "Publishing antenna_motor: " + message.data << endl;
 }
 
-void RobotInterface::antennaRetract() {
-    cout << "Testing antenna" << endl;
+void RobotInterface::antenna_motor_retract() {
+    cout << "Testing antenna_motor" << endl;
 
     // Output is in "revolutions"
     doublerevolutionsOutput = 0; // we don't know the actual position it should be. this is a placeholder
 
-    publisher_ = robotPublishers['antenna'];
+    publisher_ = robotPublishers['antenna_motor'];
     auto message = std_msgs::msg::String();
     message.data = std::to_string(revolutionsOutput);
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
-    cout << "Publishing antenna: " + message.data << endl;
+    cout << "Publishing antenna_motor: " + message.data << endl;
 }
 
-void RobotInterface::antennaTurntable(const double amount) {
-    cout << "Testing antennaTurntable" << endl;
+void RobotInterface::antenna_turntable_motor(const double amount) {
+    cout << "Testing antenna_turntable_motor" << endl;
 
     // Converting from input "degrees" to output "revolutions"
     double revolutionsOutput = amount / 360
 
-    publisher_ = robotPublishers['antennaTurntable'];
+    publisher_ = robotPublishers['antenna_turntable_motor'];
     auto message = std_msgs::msg::String();
     message.data = std::to_string(revolutionsOutput);
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
-    cout << "Publishing antennaTurntable: " + message.data << endl;
+    cout << "Publishing antenna_turntable_motor: " + message.data << endl;
 }
