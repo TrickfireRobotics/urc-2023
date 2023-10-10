ce1e08661620db909760cfff9fed90c3ea9eb17c
VladimirKupryukhin <vlad@vlad-laptop>
2023-10-09 17:02:50 -0700

    Added video test

--- src/camera_subscriber_test/camera_subscriber_test/roscamerasub.py
+++ src/camera_subscriber_test/camera_subscriber_test/roscamerasub.py
@@ -1,5 +1,8 @@
 import rclpy
 from rclpy.node import Node
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
 
 
 class RosCameraSub(Node):
@@ -7,7 +10,26 @@ class RosCameraSub(Node):
     def __init__(self):
         super().__init__("ros_camera_sub")
         self.get_logger().info("Launching ros_camera_sub node")
-
+        
+        self.subscription = self.create_subscription(
+            Image,
+            "video_frames",
+            self.listerner_callback,
+            10
+        )
+        
+        self.subscription
+        
+        self.br = CvBridge()
+        
+    def listerner_callback(self, data):
+        self.get_logger().info("Getting video frame")
+        
+        current_frame = self.br.imgmsg_to_cv2(data)
+        
+        cv2.imshow("camera", current_frame)
+        
+        cv2.waitKey(1)
 
 
     
