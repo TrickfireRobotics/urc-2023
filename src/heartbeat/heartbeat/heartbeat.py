c7f7cbf4bba509038d61eb03e385aca501194280
agrach <agrach@uw.edu>
2024-01-11 22:37:26 +0000

    Rebased onto main

--- src/heartbeat/heartbeat/heartbeat.py
+++ src/heartbeat/heartbeat/heartbeat.py
@@ -41,3 +41,18 @@ def main(args=None):
 
 if __name__ == '__main__':
     Heartbeat.main()
+import rclpy
+from rclpy.node import Node
+
+class Heartbeat(Node):
+    def __init__(self):
+        super().__init__("heartbeat_node")
+        self.get_logger().info("Hi from heartbeat!")
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = Heartbeat()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+    
--- src/viator_launch/launch/robot.launch.py
+++ src/viator_launch/launch/robot.launch.py
@@ -41,6 +41,25 @@ dummy_node = Node(
     name='dummy_node'
 )
 # ------------------------
+robot_info_node = Node(
+    package='robot_info',
+    executable='listener',
+    name='TestSubscriber'
+)
+
+robot_info_node_talker = Node(
+    package='robot_info',
+    executable='talker',
+    name='TestPublisher'
+)
+
+# -----------------------
+heartbeat_node = Node(
+    package='heartbeat',
+    executable='heartbeat',
+    name='heartbeat_node'
+)
+# ------------------------
 
 # Composable Nodes launched in a Composable Node container will share a process
 # and can use very fast inter-process communication instead of publishing
@@ -63,8 +82,6 @@ def generate_launch_description():
     return launch.LaunchDescription([
         robot_container,
         can_moteus_node,
-        heartbeat_node,
-        dummy_node,
         robot_info_node,
         robot_info_node_talker
     ])
