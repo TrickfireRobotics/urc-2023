ab5ab7882c1d103e8219059a8d016868cc85006e
K1aymore <klaymorer@protonmail.com>
2024-02-29 15:03:45 -0800

    launch_only_docker.sh runs launch.sh automatically

--- launch_only_docker.sh
+++ launch_only_docker.sh
@@ -1,3 +1,4 @@
 #Bind the host machine's urc-2023 root folder to the container's target path.
 #Launch the "trickfireimage" image by giving shell control over to the container
-docker run --mount type=bind,source="$(pwd)",target="/home/trickfire/urc-2023" -it --privileged --network=host trickfireimage
+#Launch the robot code
+docker run --mount type=bind,source="$(pwd)",target="/home/trickfire/urc-2023" -it --privileged --network=host trickfireimage /home/trickfire/urc-2023/launch.sh
