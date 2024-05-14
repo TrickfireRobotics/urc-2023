90eae12adf8a20a022abc10586893a0e9260b910
Vladimir Kupryukhin <vladimirkupryukhin@gmail.com>
2024-05-14 06:11:27 +0000

    comment out drivebase motors

--- src/can_moteus/can_moteus/ros_moteus_bridge.py
+++ src/can_moteus/can_moteus/ros_moteus_bridge.py
@@ -24,47 +24,47 @@ class RosMotuesBridge(Node):
 
         # DRIVE BASE
         moteusPubList = [moteus.Register.VELOCITY]
-        moteusMultiprocess.addMotor(
-            20,
-            "rear_right_drive_motor",
-            moteus_motor.Mode.VELOCITY,
-            moteusPubList,
-        )
+        # moteusMultiprocess.addMotor(
+        #     20,
+        #     "rear_right_drive_motor",
+        #     moteus_motor.Mode.VELOCITY,
+        #     moteusPubList,
+        # )
         
-        moteusMultiprocess.addMotor(
-            21,
-            "mid_right_drive_motor",
-            moteus_motor.Mode.VELOCITY,
-            moteusPubList,
-        )
+        # moteusMultiprocess.addMotor(
+        #     21,
+        #     "mid_right_drive_motor",
+        #     moteus_motor.Mode.VELOCITY,
+        #     moteusPubList,
+        # )
         
-        moteusMultiprocess.addMotor(
-            22,
-            "front_right_drive_motor",
-            moteus_motor.Mode.VELOCITY,
-            moteusPubList,
-        )
+        # moteusMultiprocess.addMotor(
+        #     22,
+        #     "front_right_drive_motor",
+        #     moteus_motor.Mode.VELOCITY,
+        #     moteusPubList,
+        # )
         
-        moteusMultiprocess.addMotor(
-            23,
-            "rear_left_drive_motor",
-            moteus_motor.Mode.VELOCITY,
-            moteusPubList,
-        )
+        # moteusMultiprocess.addMotor(
+        #     23,
+        #     "rear_left_drive_motor",
+        #     moteus_motor.Mode.VELOCITY,
+        #     moteusPubList,
+        # )
 
-        moteusMultiprocess.addMotor(
-            24,
-            "mid_left_drive_motor",
-            moteus_motor.Mode.VELOCITY,
-            moteusPubList,
-        )
+        # moteusMultiprocess.addMotor(
+        #     24,
+        #     "mid_left_drive_motor",
+        #     moteus_motor.Mode.VELOCITY,
+        #     moteusPubList,
+        # )
         
-        moteusMultiprocess.addMotor(
-            25,
-            "front_left_drive_motor",
-            moteus_motor.Mode.VELOCITY,
-            moteusPubList,
-        )
+        # moteusMultiprocess.addMotor(
+        #     25,
+        #     "front_left_drive_motor",
+        #     moteus_motor.Mode.VELOCITY,
+        #     moteusPubList,
+        # )
 
 
         # ARM
