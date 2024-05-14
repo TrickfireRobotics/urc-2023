f8adcbfdd388bc5bc5bbdcf29f5887f83ed39f10
Vladimir Kupryukhin <vladimirkupryukhin@gmail.com>
2024-05-14 05:53:58 +0000

    Added controller support

--- src/unveil_arm/unveil_arm/unveil_arm.py
+++ src/unveil_arm/unveil_arm/unveil_arm.py
@@ -1,11 +1,95 @@
 import rclpy
 from rclpy.node import Node
 
+import sys
+sys.path.append("/home/trickfire/urc-2023/src")
+
+from interface.robot_interface import RobotInterface
+
+from std_msgs.msg import String, Float32
+
 class UnveilArm(Node):
 
     def __init__(self):
         super().__init__("unveil_arm_node")
         self.get_logger().info("Launching unveil_arm node")
+
+        # Left wrist motor
+        self.left_wrist_motor_sub = self.create_subscription(
+            Float32, "left_wrist_cw", self.left_wrist_cw, 10)
+        self.left_wrist_motor_sub = self.create_subscription(
+            Float32, "left_wrist_ccw", self.left_wrist_ccw, 10)
+        
+        # Right wrist motor
+        self.left_wrist_motor_sub = self.create_subscription(
+            Float32, "right_wrist_cw", self.right_wrist_cw, 10)
+        self.left_wrist_motor_sub = self.create_subscription(
+            Float32, "right_wrist_ccw", self.right_wrist_ccw, 10)
+        
+        #Elbow motor
+        self.elbow_up_sub = self.create_subscription(
+            Float32, "elbow_up", self.elbow_up, 10)
+        
+        self.elbow_down_sub = self.create_subscription(
+            Float32, "elbow_down", self.elbow_down, 10)
+        
+        #Shoulder motor
+        self.shoulder_up_sub = self.create_subscription(
+            Float32, "shoulder_up", self.shoulder_up, 10)
+        
+        self.elboshoulder_down_sub = self.create_subscription(
+            Float32, "shoulder_down", self.shoulder_down, 10)
+
+
+
+    def left_wrist_cw(self, msg):
+        joystick = msg.data
+
+        if joystick > 0:
+            self.get_logger().info("Left Wrist CW" + str(joystick))
+
+    def left_wrist_ccw(self, msg):
+        joystick = msg.data
+        if joystick > 0:
+            self.get_logger().info("Left Wrist CCW" + str(joystick))
+
+    def right_wrist_cw(self, msg):
+        joystick = msg.data
+
+        if joystick > 0:
+            self.get_logger().info("Right Wrist CW" + str(joystick))
+
+    def right_wrist_ccw(self, msg):
+        joystick = msg.data
+
+        if joystick > 0:
+            self.get_logger().info("Right Wrist CCW" + str(joystick))
+
+    def elbow_up(self, msg):
+        data = msg.data
+
+        if data > 0:
+            self.get_logger().info("Elbow up" + str(data))
+
+    def elbow_down(self, msg):
+        data = msg.data
+
+        if data > 0:
+            self.get_logger().info("Elbow down" + str(data))
+
+
+    def shoulder_up(self, msg):
+        data = msg.data
+
+        if data > 0:
+            self.get_logger().info("Shoulder up" + str(data))
+
+    def shoulder_down(self, msg):
+        data = msg.data
+
+        if data > 0:
+            self.get_logger().info("Shoulder down" + str(data))
+    
 
 
 
