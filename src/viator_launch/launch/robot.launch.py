69334bd5446e42785bac7ae5ad7856a722eab976
Vladimir Kupryukhin <vladimirkupryukhin@gmail.com>
2024-06-24 16:34:56 +0000

    Add launch file modifications

--- src/viator_launch/launch/robot.launch.py
+++ src/viator_launch/launch/robot.launch.py
@@ -35,26 +35,25 @@ drivebase_node = Node(
 
 
 
-# Composable Nodes launched in a Composable Node container will share a process
-# and can use very fast inter-process communication instead of publishing
-# messages over a network socket.
-# Note: "Composable Node container" does not mean "Docker-like container".
-robot_container = ComposableNodeContainer(
-    name='robot',
-    package='rclcpp_components',
-    namespace='',
-    executable='component_container',
-    composable_node_descriptions=[
-        hello_node
-    ],
-    output='screen',
-    emulate_tty=True
-)
+# # Composable Nodes launched in a Composable Node container will share a process
+# # and can use very fast inter-process communication instead of publishing
+# # messages over a network socket.
+# # Note: "Composable Node container" does not mean "Docker-like container".
+# robot_container = ComposableNodeContainer(
+#     name='robot',
+#     package='rclcpp_components',
+#     namespace='',
+#     executable='component_container',
+#     composable_node_descriptions=[
+#         hello_node
+#     ],
+#     output='screen',
+#     emulate_tty=True
+# )
 
 
 def generate_launch_description():
     return launch.LaunchDescription([
-        robot_container,
         can_moteus_node,
         #testing_node,
         drivebase_node
