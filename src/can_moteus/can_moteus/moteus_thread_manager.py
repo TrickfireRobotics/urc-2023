9a57ae1e66a96f6f4517a11d859e30fbfb7b5001
TrickFire Robotics <trickfirerobotics@gmail.com>
2024-08-14 18:58:19 -0700

    READING POWER WORKS WOOO

--- src/can_moteus/can_moteus/moteus_thread_manager.py
+++ src/can_moteus/can_moteus/moteus_thread_manager.py
@@ -166,7 +166,10 @@ class MoteusThreadManager():
         self._shouldReconnect = False
         
         for key, moteusMotor in self._nameToMoteusMotor.items():
-            controller = moteus.Controller(moteusMotor.canID)
+            qr = moteus.QueryResolution()
+            qr.power = moteus.F32
+            controller = moteus.Controller(moteusMotor.canID, query_resolution=qr)
+            #controller = moteus.Controller(moteusMotor.canID)
             
             
             try:
