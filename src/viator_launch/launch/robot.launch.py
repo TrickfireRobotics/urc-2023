c57e7ad0014aff3730d21ef7045cc3b730d8d169
Vladimir Kupryukhin <vladimirkupryukhin@gmail.com>
2024-09-10 17:00:18 +0000

    Remove unused packages, remove unused docs, remove unused shell files

--- src/viator_launch/launch/robot.launch.py
+++ src/viator_launch/launch/robot.launch.py
@@ -1,10 +1,19 @@
 import launch
 from launch_ros.actions import Node
 
-can_moteus_node = Node(package="can_moteus", executable="can_moteus", name="can_moteus_node")
+
+can_moteus_node = Node(
+    package='can_moteus',
+    executable='can_moteus',
+    name='can_moteus_node'
+)
 
 
-drivebase_node = Node(package="drivebase", executable="drivebase", name="drivebase_node")
+drivebase_node = Node(
+    package='drivebase',
+    executable='drivebase',
+    name='drivebase_node'
+)
 
 
 mission_control_updater_node = Node(
@@ -13,13 +22,11 @@ mission_control_updater_node = Node(
     name="mission_control_updater_node",
 )
 
-arm_node = Node(package="arm", executable="arm", name="arm_node")
-
-camera_node = Node(package="camera", executable="roscamera", name="camera_node")
-
-# This is the example node. It will show ROS timers, subscribers, and publishers
-# To include it in the startup, add it to the array in the generate_launch_description() method
-example_node = Node(package="example_node", executable="myExampleNode", name="my_example_node")
+arm_node = Node(
+    package='arm',
+    executable='arm',
+    name='arm_node'
+)
 
 
 def generate_launch_description() -> launch.LaunchDescription:  # pylint: disable=invalid-name
