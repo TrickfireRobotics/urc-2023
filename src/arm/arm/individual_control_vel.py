6c78982e53986977f0ceafc8cd4a8b99a331cf47
TrickFire Robotics <trickfirerobotics@gmail.com>
2024-11-25 15:17:39 -0800

    Final Luncheon code

--- src/arm/arm/individual_control_vel.py
+++ src/arm/arm/individual_control_vel.py
@@ -176,7 +176,7 @@ class IndividualControlVel:
 
         if data > 0:
             self._ros_node.get_logger().info("Turntable clock wise")
-            self.bot_interface.runMotorSpeed(MotorConfigs.ARM_TURNTABLE_MOTOR, self.VEL)
+            self.bot_interface.runMotorSpeed(MotorConfigs.ARM_TURNTABLE_MOTOR, self.VEL * 0.5)
 
         else:
             self._ros_node.get_logger().info("Turntable STOP")
