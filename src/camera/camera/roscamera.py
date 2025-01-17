5b1b786f5337a7650feafbf3b5c32e8b2367bbdf
uellenberg <JonahUellenberg@hotmail.com>
2025-01-17 23:01:05 +0000

    Remove old camera on/off functionality

--- src/camera/camera/roscamera.py
+++ src/camera/camera/roscamera.py
@@ -4,7 +4,6 @@ from cv_bridge import CvBridge  # Package to convert between ROS and OpenCV Imag
 from rclpy.executors import MultiThreadedExecutor
 from rclpy.node import Node  # Handles the creation of nodes
 from sensor_msgs.msg import CompressedImage  # Image is the message type
-from std_msgs.msg import String
 
 
 def getCameras() -> list[int]:
@@ -43,17 +42,6 @@ class RosCamera(Node):
         self._publisher = self.create_publisher(CompressedImage, topicName, 10)
         self.get_logger().info("Created Publisher " + topicName)
 
-        # Create a subscription to read 'camera_control' topic from
-        # mission control. Topic messages are Strings.
-        #        ex. topic message: "0 off", "0 on"
-        # The topic message should always have the corresponding camera number
-        # at the first index of each message.
-        sub_topic_name = "camera_control"
-        self.subscription = self.create_subscription(
-            String, sub_topic_name, self.setCameraState, 10
-        )
-        self.get_logger().info("Created Subscription to" + sub_topic_name)
-
         # We will publish a message every 0.1 seconds
         # timer_period = 0.1  # seconds
         timer_period = 0.1  # seconds
@@ -73,7 +61,6 @@ class RosCamera(Node):
 
         # Declare parameters for each 'ros_camera' node thread
         self.declare_parameter("cameraNumber", camera)
-        self.declare_parameter("isOn", True)
 
     def publishCameraFrame(self) -> None:
         """
@@ -85,49 +72,9 @@ class RosCamera(Node):
         # This method returns True/False as well as the video frame.
         ret, frame = self.cap.read()
 
-        # Get bool parameter, isOn, and check the state of the camera to publish frame or not
-        # Value of isOn is controlled by setCameraState()
-        is_on = bool(self.get_parameter("isOn")._value)
-        if ret and is_on:
+        if ret:
             # Publish the image.
             self._publisher.publish(self.br.cv2_to_compressed_imgmsg(frame))
-
-    def setCameraState(self, msg: String) -> None:
-        """
-        Function reads str msgs from topic, 'camera_control' (published by mission control), and
-        turns on/off the periodic publishing of frames by a certain camera
-
-        Function is called whenever a msg is published to 'camera_control'
-
-        Function assumes str msgs sent to 'camera_control' begin with a number and ends with an 'f'
-        (meaning off) or 'n' (meaning on)
-            ex.
-                "0 off" -> to turn off camera with id=0
-                "2 on" -> to turn on camera with id=2
-        """
-
-        # Check if this specific camera is targeted by mission control, stop the function otherwise
-        camera_number = int(self.get_parameter("cameraNumber")._value)
-        if int(msg.data[0]) != camera_number:
-            return
-
-        # Check if the desired camera state is already fulfilled, print and stop the function if so
-        camera_on = bool(self.get_parameter("isOn")._value)
-        if str(msg.data[-1]) == "f" and not camera_on:
-            self.get_logger().info("camera" + str(camera_number) + " is already off!")
-            return
-        elif str(msg.data[-1]) == "n" and camera_on:
-            self.get_logger().info("camera" + str(camera_number) + " is already on!")
-            return
-
-        # Set isOn bool parameter to either stop or restart the publishing of frames
-        self.undeclare_parameter("isOn")
-        if str(msg.data[-1]) == "f":
-            # self.get_logger().info("turning camera" + str(cameraNumber) + " off!")
-            self.declare_parameter("isOn", False)
-        else:
-            # self.get_logger().info("turning camera" + str(cameraNumber) + " on!")
-            self.declare_parameter("isOn", True)
 
 
 def main(args: list[str] | None = None) -> None:
