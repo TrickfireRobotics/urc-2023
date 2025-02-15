4502a7184c8e5910db625915bea4d7c77cb1e5ba
Arya Khubcher <ecstaticlimb@gmail.com>
2025-02-15 02:35:15 +0000

    [Motherboard] [MakeInterfaceWork] edited robot interface and config file

--- src/lib/configs.py
+++ src/lib/configs.py
@@ -43,6 +43,119 @@ class MoteusMotorConfig:
         """
         return f"moteusmotor_{self.can_id}_from_interface"
 
+@dataclass(frozen=True)
+class LightConfig:
+    """
+    A dataclass that contains config values relating to lights connected via the can bus.
+    """
+
+    can_id: int
+    """
+    The can id of the light.
+    """
+
+    config: dict[str, float | int]
+    """
+    The config of the light.
+    """
+
+    def getCanTopicName(self) -> str:
+        """
+        Gets the light's topic name for data sourced from can.
+
+        Returns the following format: `light_<can_id>_from_can`
+        """
+        return f"light_{self.can_id}_from_can"
+
+
+@dataclass(frozen=True)
+class ServoConfig:
+    """
+    A dataclass that contains config values relating to servos connected via the can bus.
+    """
+
+    can_id: int
+    """
+    The can id of the servo.
+    """
+
+    config: dict[str, float | int]
+    """
+    The config of the servo.
+    """
+
+    def getCanTopicName(self) -> str:
+        """
+        Gets the servo's topic name for data sourced from can.
+
+        Returns the following format: `servo_<can_id>_from_can`
+        """
+        return f"servo_{self.can_id}_from_can"
+
+
+@dataclass(frozen=True)
+class StepperConfig:
+    """
+    A dataclass that contains config values relating to stepper motors connected via the can bus.
+    """
+
+    can_id: int
+    """
+    The can id of the stepper motor.
+    """
+
+    config: dict[str, float | int]
+
+    ms1: int 
+    ms2: int 
+    ms3: int 
+
+    """
+    The config of the stepper motor.
+    """
+
+    def getCanTopicName(self) -> str:
+        """
+        Gets the stepper motor's topic name for data sourced from can.
+
+        Returns the following format: `stepper_<can_id>_from_can`
+        """
+        return f"stepper_{self.can_id}_from_can"
+
+
+@dataclass(frozen=True)
+class MotherboardConfig:
+    """
+    A dataclass that contains variable names and configuration IDs for motherboard components.
+    """
+    # Light configurations
+    LIGHT_RED = "red"
+    LIGHT_GREEN = "green"
+    LIGHT_BLUE = "blue"
+    LIGHT_BRIGHTNESS = "brightness"
+    LIGHT_FREQUENCY_HZ = "frequency_hz"
+
+    # Servo configurations
+    SERVO_0 = "servo_0"
+    SERVO_1 = "servo_1"
+    SERVO_2 = "servo_2"
+    SERVO_3 = "servo_3"
+    SERVO_4 = "servo_4"
+    SERVO_5 = "servo_5"
+    SERVO_PWM_RATE_HZ = "pwm_rate_hz"
+    SERVO_MAX_ANGLE = "max_angle"
+    SERVO_MIN_ANGLE = "min_angle"
+
+    # Stepper configurations
+    STEPPER_0 = "stepper_0"
+    STEPPER_1 = "stepper_1"
+    STEPPER_2 = "stepper_2"
+    STEPPER_3 = "stepper_3"
+    STEPPER_4 = "stepper_4"
+    STEPPER_5 = "stepper_5"
+    STEPPER_MAX_STEPS = "max_steps"
+    STEPPER_STEP_RATE = "step_rate"
+
 
 class MotorConfigs:
     """
@@ -175,6 +288,148 @@ class MotorConfigs:
         },
     )
 
+    # Lights
+    LIGHT_RED = LightConfig(
+        can_id=30,
+        config={
+            MotherboardConfig.LIGHT_BRIGHTNESS: 255,
+            MotherboardConfig.LIGHT_FREQUENCY_HZ: 100,
+        },
+    )
+    LIGHT_GREEN = LightConfig(
+        can_id=31,
+        config={
+            MotherboardConfig.LIGHT_BRIGHTNESS: 255,
+            MotherboardConfig.LIGHT_FREQUENCY_HZ: 100,
+        },
+    )
+    LIGHT_BLUE = LightConfig(
+        can_id=32,
+        config={
+            MotherboardConfig.LIGHT_BRIGHTNESS: 255,
+            MotherboardConfig.LIGHT_FREQUENCY_HZ: 100,
+        },
+    )
+
+    # Servos
+    SERVO_0 = ServoConfig(
+        can_id=40,
+        config={
+            MotherboardConfig.SERVO_PWM_RATE_HZ: 50,
+            MotherboardConfig.SERVO_MAX_ANGLE: 180.0,
+            MotherboardConfig.SERVO_MIN_ANGLE: 0.0,
+        },
+    )
+    SERVO_1 = ServoConfig(
+        can_id=41,
+        config={
+            MotherboardConfig.SERVO_PWM_RATE_HZ: 50,
+            MotherboardConfig.SERVO_MAX_ANGLE: 180.0,
+            MotherboardConfig.SERVO_MIN_ANGLE: 0.0,
+        },
+    )
+    SERVO_2 = ServoConfig(
+        can_id=42,
+        config={
+            MotherboardConfig.SERVO_PWM_RATE_HZ: 50,
+            MotherboardConfig.SERVO_MAX_ANGLE: 180.0,
+            MotherboardConfig.SERVO_MIN_ANGLE: 0.0,
+        },
+    )
+    SERVO_3 = ServoConfig(
+        can_id=43,
+        config={
+            MotherboardConfig.SERVO_PWM_RATE_HZ: 50,
+            MotherboardConfig.SERVO_MAX_ANGLE: 180.0,
+            MotherboardConfig.SERVO_MIN_ANGLE: 0.0,
+        },
+    )
+    SERVO_4 = ServoConfig(
+        can_id=44,
+        config={
+            MotherboardConfig.SERVO_PWM_RATE_HZ: 50,
+            MotherboardConfig.SERVO_MAX_ANGLE: 180.0,
+            MotherboardConfig.SERVO_MIN_ANGLE: 0.0,
+        },
+    )
+    SERVO_5 = ServoConfig(
+        can_id=45,
+        config={
+            MotherboardConfig.SERVO_PWM_RATE_HZ: 50,
+            MotherboardConfig.SERVO_MAX_ANGLE: 180.0,
+            MotherboardConfig.SERVO_MIN_ANGLE: 0.0,
+        },
+    )
+
+    # Steppers
+    STEPPER_0 = StepperConfig(
+        can_id=50,
+        config={
+            MotherboardConfig.STEPPER_MAX_STEPS: 200,
+            MotherboardConfig.STEPPER_STEP_RATE: 1000,
+        },
+
+        ms1=0,
+        ms2=0,
+        ms3=0,
+    )
+    STEPPER_1 = StepperConfig(
+        can_id=51,
+        config={
+            MotherboardConfig.STEPPER_MAX_STEPS: 200,
+            MotherboardConfig.STEPPER_STEP_RATE: 1000,
+        },
+
+        ms1=0,
+        ms2=0,
+        ms3=0,
+    )
+    STEPPER_2 = StepperConfig(
+        can_id=52,
+        config={
+            MotherboardConfig.STEPPER_MAX_STEPS: 200,
+            MotherboardConfig.STEPPER_STEP_RATE: 1000,
+        },
+
+        ms1=0,
+        ms2=0,
+        ms3=0,
+    )
+    STEPPER_3 = StepperConfig(
+        can_id=53,
+        config={
+            MotherboardConfig.STEPPER_MAX_STEPS: 200,
+            MotherboardConfig.STEPPER_STEP_RATE: 1000,
+        },
+
+        ms1=0,
+        ms2=0,
+        ms3=0,
+    )
+    STEPPER_4 = StepperConfig(
+        can_id=54,
+        config={
+            MotherboardConfig.STEPPER_MAX_STEPS: 200,
+            MotherboardConfig.STEPPER_STEP_RATE: 1000,
+        },
+
+        ms1=0,
+        ms2=0,
+        ms3=0,
+    )
+    STEPPER_5 = StepperConfig(
+        can_id=55,
+        config={
+            MotherboardConfig.STEPPER_MAX_STEPS: 200,
+            MotherboardConfig.STEPPER_STEP_RATE: 1000,
+        },
+
+        ms1=0,
+        ms2=0,
+        ms3=0,
+    )
+
+
     # Don't allow anyone to change this class's attributes
     def __setattr__(self, _: str, __: Any) -> Any:
         raise AttributeError("Trying to set attribute on a frozen instance")
@@ -189,4 +444,37 @@ class MotorConfigs:
             value
             for key, value in cls.__dict__.items()
             if not key.startswith("_") and key != "getAllMotors"
+        ]
+    
+    @classmethod
+    def getAllLights(cls) -> list[LightConfig]:     
+        """
+        Returns a list of every light in this constants class.
+        """
+        return [
+            value
+            for key, value in cls.__dict__.items()
+            if not key.startswith("_") and isinstance(value, LightConfig)
+        ]
+
+    @classmethod
+    def getAllServos(cls) -> list[ServoConfig]:
+        """
+        Returns a list of every servo in this constants class.
+        """
+        return [
+            value
+            for key, value in cls.__dict__.items()
+            if not key.startswith("_") and isinstance(value, ServoConfig)
+        ]
+
+    @classmethod
+    def getAllSteppers(cls) -> list[StepperConfig]:
+        """
+        Returns a list of every stepper motor in this constants class.
+        """
+        return [
+            value
+            for key, value in cls.__dict__.items()
+            if not key.startswith("_") and isinstance(value, StepperConfig)
         ]
--- src/lib/interface/robot_interface.py
+++ src/lib/interface/robot_interface.py
@@ -9,7 +9,7 @@ from rclpy.node import Node
 from rclpy.publisher import Publisher
 from std_msgs.msg import String
 
-from lib.configs import MoteusMotorConfig, MotorConfigs
+from lib.configs import MoteusMotorConfig, MotorConfigs, LightConfig, ServoConfig, StepperConfig
 from lib.moteus_motor_state import MoteusRunSettings
 
 REVS_TO_RADIANS = math.pi * 2
@@ -30,6 +30,24 @@ class RobotInterface:
                 String, motor_config.getInterfaceTopicName(), 10
             )
 
+        # Initialize publishers for lights
+        for light_config in MotorConfigs.getAllLights():
+            self._publishers[light_config.can_id] = self._ros_node.create_publisher(
+                String, light_config.getCanTopicName(), 10
+            )
+
+        # Initialize publishers for servos
+        for servo_config in MotorConfigs.getAllServos():
+            self._publishers[servo_config.can_id] = self._ros_node.create_publisher(
+                String, servo_config.getCanTopicName(), 10
+            )
+
+        # Initialize publishers for steppers
+        for stepper_config in MotorConfigs.getAllSteppers():
+            self._publishers[stepper_config.can_id] = self._ros_node.create_publisher(
+                String, stepper_config.getCanTopicName(), 10
+            )
+            
     def runMotor(self, motor: MoteusMotorConfig, run_settings: MoteusRunSettings) -> None:
         """
         Runs the specified motor with the specified settings.
@@ -101,3 +119,94 @@ class RobotInterface:
             The config of the motor to disable.
         """
         self.runMotor(motor, MoteusRunSettings(velocity=0, set_stop=True))
+
+    # ------ LIGHTS ------
+    def setLightState(self, light: LightConfig, state: str) -> bool:
+        """
+        Sets the state of the specified light.
+
+        Parameters
+        -------
+        light : LightConfig
+            The config of the light to set the state for.
+        state : str
+            The state to set the light to. Should be either "TURN_ON" or "TURN_OFF".
+
+        Returns
+        -------
+        bool
+            True if the command was sent successfully, False otherwise.
+        """
+        if light.can_id in self._publishers and state in ["TURN_ON", "TURN_OFF"]:
+            str_msg = String()
+            str_msg.data = state
+            self._publishers[light.can_id].publish(str_msg)
+            return True
+        return False
+
+    # ------ Steppers ------
+    def setStepperMotorState(self, stepper: StepperConfig, command: str, position: float = math.nan, speed: float = math.nan) -> bool:
+        """
+        Sets the state of the specified stepper motor.
+
+        Parameters
+        -------
+        stepper : StepperConfig
+            The config of the stepper motor to set the state for.
+        command : str
+            The command to set the stepper motor to. Should be one of "RUN", "SPEED", "POSITION", "STOP", "DISABLE".
+        position : float, optional
+            The target position for the stepper motor, if applicable.
+        speed : float, optional
+            The target speed for the stepper motor, if applicable.
+
+        Returns
+        -------
+        bool
+            True if the command was sent successfully, False otherwise.
+        """
+        if stepper.can_id in self._publishers and command in ["RUN", "SPEED", "POSITION", "STOP", "DISABLE"]:
+            str_msg = String()
+            if command == "SPEED" and not math.isnan(speed):
+                str_msg.data = f"{command}:{speed}"
+            elif command == "POSITION" and not math.isnan(position):
+                str_msg.data = f"{command}:{position}"
+            else:
+                str_msg.data = command
+            self._publishers[stepper.can_id].publish(str_msg)
+            return True
+        return False
+
+    # ------ Servos ------
+    def setServoMotorState(self, servo: ServoConfig, command: str, position: float = math.nan, speed: float = math.nan) -> bool:
+        """
+        Sets the state of the specified servo motor.
+
+        Parameters
+        -------
+        servo : ServoConfig
+            The config of the servo motor to set the state for.
+        command : str
+            The command to set the servo motor to. Should be one of "RUN", "SPEED", "POSITION", "STOP", "DISABLE".
+        position : float, optional
+            The target position for the servo motor, if applicable.
+        speed : float, optional
+            The target speed for the servo motor, if applicable.
+
+        Returns
+        -------
+        bool
+            True if the command was sent successfully, False otherwise.
+        """
+        if servo.can_id in self._publishers and command in ["RUN", "SPEED", "POSITION", "STOP", "DISABLE"]:
+            str_msg = String()
+            if command == "SPEED" and not math.isnan(speed):
+                str_msg.data = f"{command}:{speed}"
+            elif command == "POSITION" and not math.isnan(position):
+                str_msg.data = f"{command}:{position}"
+            else:
+                str_msg.data = command
+            self._publishers[servo.can_id].publish(str_msg)
+            return True
+        return False
+    
