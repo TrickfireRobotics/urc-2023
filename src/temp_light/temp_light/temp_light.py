5ee1973dcfbab6d84a2448fca79eb07dc3b311fa
TrickFire Robotics <trickfirerobotics@gmail.com>
2025-02-21 15:04:24 -0800

    [autonav][templights] Lights work

--- src/temp_light/temp_light/temp_light.py
+++ src/temp_light/temp_light/temp_light.py
@@ -2,7 +2,7 @@ import json
 import sys
 
 import rclpy
-import RPi.GPIO as GPIO
+import Jetson.GPIO as GPIO
 from rclpy.executors import ExternalShutdownException
 from rclpy.node import Node
 from std_msgs.msg import String
@@ -11,9 +11,9 @@ from lib.color_codes import ColorCodes, colorStr
 from lib.configs import MotorConfigs
 from lib.interface.robot_interface import RobotInterface
 
-RED_PIN_NUM = 32  # GPIO32, physical pin 13
-GREEN_PIN_NUM = 27  # GPIO27, physical pin 15
-BLUE_PIN_NUM = 8  # GPIO08, physical pin16
+RED_PIN_NUM = 15  
+GREEN_PIN_NUM = 13  
+BLUE_PIN_NUM = 16  
 
 
 class TempLight(Node):
@@ -25,18 +25,22 @@ class TempLight(Node):
             String, "temp_light_from_interface", self._handle_temp_light, 10
         )
 
-        GPIO.setmode(GPIO.BCM)  # BCM pin-numbering scheme from Raspberry Pi
-        GPIO.setup(RED_PIN_NUM, GPIO.OUT, initial=GPIO.HIGH)
-        GPIO.setup(GREEN_PIN_NUM, GPIO.OUT, initial=GPIO.HIGH)
-        GPIO.setup(BLUE_PIN_NUM, GPIO.OUT, initial=GPIO.HIGH)
+        GPIO.setmode(GPIO.BOARD)  # BCM pin-numbering scheme from Raspberry Pi
+        GPIO.setup(RED_PIN_NUM, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(GREEN_PIN_NUM, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(BLUE_PIN_NUM, GPIO.OUT, initial=GPIO.LOW)
 
-        # self.bot_interface = RobotInterface(self)
-
-        # self.timer = self.create_timer(0.5, self.timer_callback)
+        self.bot_interface = RobotInterface(self)
+        self.hello = 1
+        self.timer = self.create_timer(2, self.timer_callback)
 
     def _handle_temp_light(self, msg: String) -> None:
         light_result = json.loads(msg.data)
-        state = light_result["state"]
+        state = GPIO.LOW
+        if light_result["state"] == 0:
+            state = GPIO.LOW
+        else :
+            state = GPIO.HIGH
 
         # LOW all pins
         GPIO.output(RED_PIN_NUM, GPIO.LOW)
@@ -51,8 +55,16 @@ class TempLight(Node):
             GPIO.output(BLUE_PIN_NUM, state)
 
     def timer_callback(self) -> None:
-        self.bot_interface.setTempLight("GREEN", 0)
-
+        if self.hello == 0:
+            self.bot_interface.setTempLight("RED", 1)
+        elif self.hello == 1:
+            self.bot_interface.setTempLight("GREEN", 1)
+        else:
+            self.bot_interface.setTempLight("BLUE", 1)
+        
+        self.hello = self.hello + 1    
+        self.hello = self.hello % 3
+        
 
 def main(args: list[str] | None = None) -> None:
     """
