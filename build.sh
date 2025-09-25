109e154fc9d8adf01ebf43e742f124a566a3a78a
JacobKnoth <jakek1406@gmail.com>
2025-09-25 16:57:32 -0700

    fixed build script

--- build.sh
+++ build.sh
@@ -4,6 +4,15 @@
 ./setup_zed_dependencies.sh
 
 source /opt/ros/$ROS_DISTRO/setup.bash
+
+# build zed components first
+colcon build \
+--symlink-install \
+--base-paths /home/trickfire/jake-voxel-urc/urc-2023 \
+--packages-select zed_components \
+--cmake-args \
+-DCMAKE_BUILD_TYPE=Debug
+
 colcon build \
 --symlink-install \
 --base-paths /home/trickfire/jake-voxel-urc/urc-2023 \
