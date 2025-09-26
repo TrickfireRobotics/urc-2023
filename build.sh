4082031dfa93ade05045cc5a61d93f67f58121e9
JacobKnoth <jakek1406@gmail.com>
2025-09-26 11:22:19 -0700

    Trying to debug a zed wrapper issue

--- build.sh
+++ build.sh
@@ -11,17 +11,17 @@ colcon build \
 --base-paths /home/trickfire/jake-voxel-urc/urc-2023 \
 --packages-select zed_components \
 --cmake-args \
--DCMAKE_BUILD_TYPE=Debug
+-DCMAKE_BUILD_TYPE=RelWithDebInfo
 
 colcon build \
 --symlink-install \
 --base-paths /home/trickfire/jake-voxel-urc/urc-2023 \
 --packages-select zed_wrapper \
 --cmake-args \
--DCMAKE_BUILD_TYPE=Debug
+-DCMAKE_BUILD_TYPE=RelWithDebInfo
 
 colcon build \
     --symlink-install \
     --base-paths /home/jake-voxel-urc/urc-2023\
     --cmake-args \
-        -DCMAKE_BUILD_TYPE=Debug
+        -DCMAKE_BUILD_TYPE=RelWithDebInfo
