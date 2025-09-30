437655b769ecfb65e44265578254ad7099f78aba
JacobKnoth <jakek1406@gmail.com>
2025-09-29 18:21:48 -0700

    Added to launch to auto start octomap server

--- launch.sh
+++ launch.sh
@@ -9,7 +9,12 @@ source /home/trickfire/jake-voxel-urc/urc-2023/install/setup.bash
 #ip link set can0 type can bitrate 1000000 dbitrate 5000000 fd on
 #ip link set can0 up
 
+# Launch octomap server
+ros2 run octomap_server octomap_server_node --ros-args   -r cloud_in:=/filtered_point_cloud   -p resolution:=0.05   -p frame_id:=map   -p latch:=true   -p visualize_free_space:=true
+
 # Add to the python import pathes
 export PYTHONPATH="/home/trickfire/jake-voxel-urc/urc-2023/src/:$PYTHONPATH"
 
 ros2 launch viator_launch robot.launch.py
+
+
