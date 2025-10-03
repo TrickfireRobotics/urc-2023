b61b4ff6d38470386a72ca06e6474fcc350fad66
Jonathan Gibbs <jon@gibbs.tk>
2025-10-03 19:24:39 +0000

    made octomap run in background

--- launch.sh
+++ launch.sh
@@ -10,7 +10,7 @@ source /home/trickfire/jon-merge-jake-voxel/urc-2023/install/setup.bash
 #ip link set can0 up
 
 # Launch octomap server
-ros2 run octomap_server octomap_server_node --ros-args   -r cloud_in:=/filtered_point_cloud   -p resolution:=0.05   -p frame_id:=map   -p latch:=true   -p visualize_free_space:=true
+ros2 run octomap_server octomap_server_node --ros-args   -r cloud_in:=/filtered_point_cloud   -p resolution:=0.05   -p frame_id:=map   -p latch:=true   -p visualize_free_space:=true&
 
 # Add to the python import pathes
 export PYTHONPATH="/home/trickfire/jon-merge-jake-voxel/urc-2023/src/:$PYTHONPATH"
