d7df8801f0745622fa286f4241165bc15788991e
nukkeldev <nukkel.dev@gmail.com>
2025-10-22 12:08:42 -0700

    replaced slip with effort

--- src/autonomous_nav/autonomous_nav/stuck_detection_node.py
+++ src/autonomous_nav/autonomous_nav/stuck_detection_node.py
@@ -10,13 +10,9 @@ from std_msgs.msg import Bool, String
 
 from lib.color_codes import ColorCodes, colorStr
 from lib.configs import MoteusMotorConfig, MotorConfigs
-from lib.interface.robot_info import RobotInfo
 from lib.moteus_motor_state import MoteusMotorState, MoteusRunSettings
 
 NODE_NAME = "stuck_detection_node"
-
-SLIP_THRESHOLD = 0.3
-SLIP_DURATION = 2.0
 
 
 class StuckDetectionNode(Node):
@@ -43,7 +39,8 @@ class StuckDetectionNode(Node):
         self.target_states: Dict[int, MoteusRunSettings] = {}
         self.current_states: Dict[int, MoteusMotorState] = {}
 
-        self.stuck_start: Optional[float] = None
+        self.effort_stuck_start: Optional[float] = None
+
         self.stuck_publisher = self.create_publisher(Bool, "/stuck", 10)
 
         self.timer = self.create_timer(0.1, self.update)
@@ -86,46 +83,70 @@ class StuckDetectionNode(Node):
     def motor_target_state_callback(self, can_id: int, settings: MoteusRunSettings) -> None:
         self.target_states[can_id] = settings
 
-    # Update
+    # Detection
 
-    def update(self) -> None:
-        target_velocity_sum = 0.0
-        slip_sum = 0.0
-        count = 0
+    def calculate_per_motor_effort(self) -> bool:
+        RMX8_25_RATED_TORQUE = 10  # N/m
+        FREE_SPINNING_THRESH = (0.2, 0.9)  # <t, >v
+        STUCK_THRESH = (0.5, 0.2)  # >t, <v
+        STUCK_WHEEL_MIN_COUNT = 3
+        DURATION = 1.0
+
+        stuck_count = 0
+        free_spinning_count = 0
 
         for config in self.slip_motor_configs:
             target = self.target_states.get(config.can_id)
             state = self.current_states.get(config.can_id)
 
-            if target is not None and state is not None:
-                # We should always be controlling the drive motors in velocity mode.
-                if state.velocity is None or target.velocity is None:
-                    continue
+            if target is None or state is None:
+                continue
 
-                if abs(target.velocity) < 1e-6:
-                    continue
+            if state.velocity is None or target.velocity is None:
+                continue
 
-                target_velocity_sum += target.velocity
-                slip = abs((state.velocity - target.velocity) / target.velocity)
-                slip_sum += slip
-                count += 1
+            if state.torque is None:
+                # TODO: Fallback to q_current
+                continue
 
-        if count == 0:
-            self.stuck_publisher.publish(Bool(data=False))
-            return
+            if abs(target.velocity) < 1e-3:
+                continue
+
+            v_norm = abs(state.velocity / target.velocity)
+            v_norm = min(v_norm, 1.0)
+
+            t_norm = abs(state.torque) / RMX8_25_RATED_TORQUE
+
+            if t_norm < FREE_SPINNING_THRESH[0] and v_norm > FREE_SPINNING_THRESH[1]:
+                free_spinning_count += 1
+
+            if t_norm > STUCK_THRESH[0] and v_norm < STUCK_THRESH[1]:
+                stuck_count += 1
 
         now = time.time()
-        stuck = abs(target_velocity_sum) > 1e-3 and (slip_sum / count) > SLIP_THRESHOLD
-
+        # TODO: Differentiate free-spinning vs. stuck
+        stuck = stuck_count + free_spinning_count > STUCK_WHEEL_MIN_COUNT
         if stuck:
-            if self.stuck_start is not None:
-                if now - self.stuck_start > SLIP_DURATION:
-                    self.stuck_publisher.publish(Bool(data=True))
+            if self.effort_stuck_start is not None:
+                if now - self.effort_stuck_start > DURATION:
+                    return True
             else:
-                self.stuck_start = now
+                self.effort_stuck_start = now
+        else:
+            self.effort_stuck_start = None
+
+        return False
+
+    # Update
+
+    def update(self) -> None:
+        effort_stuck = self.calculate_per_motor_effort()
+
+        # TODO: Use custom message with constants for reasons
+        if effort_stuck:
+            self.stuck_publisher.publish(Bool(data=True))
         else:
             self.stuck_publisher.publish(Bool(data=False))
-            self.stuck_start = None
 
 
 def main(args: list[str] | None = None) -> None:
