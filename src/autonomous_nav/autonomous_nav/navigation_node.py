ad372f5f6c3121b101731abd5e7c5e1233f8d385
Jonathan Gibbs <jon@gibbs.tk>
2025-10-28 16:20:47 +0000

    same thing as previous commit

--- src/autonomous_nav/autonomous_nav/navigation_node.py
+++ src/autonomous_nav/autonomous_nav/navigation_node.py
@@ -232,6 +232,7 @@ class NavigationNode(Node):
             self.append_path((1, 1))
             self.append_path((1, 2))
             self.append_path((2, 0))
+            self.path_pub.publish(self.path)
             self.get_logger().warn("RRunning path planner")
             self.planPath(self.global_costmap)
         self.publishStatus(f"En route to waypoint ({goal_x:.2f}, {goal_y:.2f})")
