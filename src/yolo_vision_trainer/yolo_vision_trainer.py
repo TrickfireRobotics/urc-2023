523926d24d9a3da3317ca8c2b7ec7a800035383b
GenericUserIsTaken <wil.tygo@gmail.com>
2026-01-23 14:50:23 -0800

    fix path and print states

--- src/yolo_vision_trainer/yolo_vision_trainer.py
+++ src/yolo_vision_trainer/yolo_vision_trainer.py
@@ -7,12 +7,15 @@ from ultralytics import YOLO
 import torch
 
 def pre_train_model():
+    print("Starting cuda check")
     # Check if GPU is available
     print("CUDA Available:", torch.cuda.is_available())
 
     # Get GPU details
     if torch.cuda.is_available():
         print("GPU Name:", torch.cuda.get_device_name(0))
+    else:
+        return
 
     # Initialize a YOLO-World model
     model = YOLO("yolov8l-world.pt")  # or select yolov8m/l-world.pt
@@ -24,5 +27,6 @@ def pre_train_model():
     # Save the model with the defined offline vocabulary
     model.save("trained_yolov8l.pt")
 
-if __name__ is "__main__":
+if __name__ == "__main__":
+    print("launching pre_training")
     pre_train_model()
