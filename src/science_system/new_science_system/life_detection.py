599dddab1f9480dd581e414720cd2c4ea6d93f5b
Matheus <matheus.werneck2006@gmail.com>
2026-01-23 16:50:05 -0800

    Added difference calculations, baseline, and fixed input bug

--- src/science_system/new_science_system/life_detection.py
+++ src/science_system/new_science_system/life_detection.py
@@ -17,7 +17,6 @@ from godirect import GoDirect
 # A lot of this code is a part of the sample code from Vernier, take the comments with a grain a salt since we do not need to use a lot of the features provided with the device
 # Im aware that there is a gdx version which is easier to use, but I could not get it working for some reason
 # I recently added multithreading to allow for easy stopping and starting but i have not been able to do extensive testing on it yet
-# I also recently made some changes to make it compatible with the repository so I hope nothing broke, since I have not been able to test it yet
 # The main plan is to show the difference between each measurement and to give the average difference over the entire measurement period (and hopefully be able to visualize it as a graph)
 
 
@@ -25,58 +24,73 @@ import logging
 import threading
 logging.basicConfig()
 
+# The first USB device found will be used. If no USB devices are found, then 
+# the BLE device with the strongest signal over -100 is used.
+godirect = GoDirect(use_ble=False, use_usb=True)
+print("GoDirect v"+str(godirect.get_version()))
+print("\nSearching...", flush=True, end ="")
+device = godirect.get_device(threshold=-100)
 stop_flag = False
 
-def get_user_input() -> None:
+def get_user_input():
     global stop_flag
     input("Press Enter to stop...\n")
     stop_flag = True
 
 input_thread = threading.Thread(target=get_user_input, daemon=True)
 
-def main(args: list[str] | None = None) -> None:
-        # The first USB device found will be used. If no USB devices are found, then 
-        # the BLE device with the strongest signal over -100 is used.
-        godirect = GoDirect(use_ble=False, use_usb=True)
-        print("GoDirect v"+str(godirect.get_version()))
-        print("\nSearching...", flush=True, end ="")
-        device = godirect.get_device(threshold=-100)
+# Once a device is found or selected it must be opened.
+if device != None and device.open(auto_start=False):
+        print("connecting.\n")
+        print("Connected to "+device.name)
 
-        # Once a device is found or selected it must be opened.
-        if device != None and device.open(auto_start=False):
-                print("connecting.\n")
-                print("Connected to "+device.name)
+        # A specific sensor (or sensors) can be selected for data collection by calling
+        # device.enable_sensors([]) prior to calling device.start().
+        # If you do not use device.enable_sensors([]), the default sensor(s) will be automatically
+        # enabled when device.start() is called. 
+        
+        # Length is the amount of measurements to take, since one measurement is taken every second, this is also the amount of seconds the program will run for
+        length = int(input("How Long? \n"))
+        if length <= 0:
+               length = 1
 
-                # A specific sensor (or sensors) can be selected for data collection by calling
-                # device.enable_sensors([]) prior to calling device.start().
-                # If you do not use device.enable_sensors([]), the default sensor(s) will be automatically
-                # enabled when device.start() is called. 
+        # Set a baseline (preferably the normal avg difference in the area) the baseline will be removed from the total showing the true average difference 
+        baseline = float(input("Baseline? (input 0 if no)\n"))
 
-                # The start period is how often it collects data in milliseconds, i would reccomend leaving it at 1000 for now as I experienced some bugs when changing the value
-                device.start(period=1000) 
-                print("start")
-                sensors = device.get_enabled_sensors()   # after start() is called, an enabled sensor list is available
-                
-                # Lenght is the amount of measurements to take, since one measurement is taken every second, this is also the amount of seconds the program will run for
-                print("How Long?")
-                length = int(input())
+        # The start period is how often it collects data in milliseconds, i would reccomend leaving it at 1000 for now as I experienced some bugs when changing the value
+        device.start(period=1000) 
+        print("start")
+        sensors = device.get_enabled_sensors()   # after start() is called, an enabled sensor list is available
 
-                print("Reading measurements\n")
-                input_thread.start()
-                for i in range(0,length):
-                        if device.read():
-                                if (stop_flag):
-                                        break  
-                                for sensor in sensors:
-                                        # The 'sensor.values' call returns a list of measurements. This list might contain 
-                                        # one sensor value, or multiple sensor values (if fast sampling)
-                                        print(sensor.sensor_description+": "+str(sensor.values))                
-                                        sensor.clear()
-                device.stop()
-                device.close()
-                print("\nDisconnected from "+device.name)
+        print("Reading measurements\n")
+        input_thread.start()
 
-        else:
-                print("Go Direct device not found/opened")
+        # Doing one check before for loop so that we can take difference easier
+        # Keep in mind that since there can be multiple sensors and multiple values both values and sensors are vectors/arrays (idk) 
+        # but we are only using one sensor with one value so our info will be at 0
+        if device.read():
+                total = 0
+                amt = sensors[0].values[0]
+                print(sensors[0].sensor_description+": "+str(sensors[0].values))
+                sensors[0].clear()
 
-        godirect.quit()
+        for i in range(1,length):
+                if device.read():
+                        if (stop_flag):
+                                break  
+                        for sensor in sensors:
+                                # The 'sensor.values' call returns a list of measurements. This list might contain 
+                                # one sensor value, or multiple sensor values (if fast sampling)
+                                print(sensor.sensor_description+": "+str(sensor.values))
+                                total += abs(amt - sensor.values[0])
+                                amt = sensor.values[0]
+                                sensor.clear()
+        device.stop()
+        device.close()
+        print("\nDisconnected from "+device.name)
+        print("Avg diff: " + str((total / (length/ 2)) - baseline))
+
+else:
+        print("Go Direct device not found/opened")
+
+godirect.quit()
