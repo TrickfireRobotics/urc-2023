a9a4ece138a55c3055155a72cd2aacfb9fc3776c
Jonathan Gibbs <jon@gibbs.tk>
2026-02-23 21:47:06 +0000

    fixed improper can id issue

--- speed_cansend.sh
+++ speed_cansend.sh
@@ -5,12 +5,12 @@ START_CMD="8800000000000000"
 CLEAR_CMD="9B00000000000000"
 SPEED_CMD="A200000000000000"
 for ID in 21 22 23 24 25 26; do
-    HEX_ID=$(printf "%02X" $ID)
-    echo "Sending to CAN ID $HEX_ID: cansend can0 ${HEX_ID}#${START_CMD}"
-    sudo cansend can0 ${HEX_ID}#${START_CMD}
-    echo "Sending to CAN ID $HEX_ID: cansend can0 ${HEX_ID}#${CLEAR_CMD}"
-    sudo cansend can0 ${HEX_ID}#${CLEAR_CMD}
-    echo "Sending to CAN ID $HEX_ID: cansend can0 ${HEX_ID}#${SPEED_CMD}"
-    sudo cansend can0 ${HEX_ID}#${SPEED_CMD}
+    CAN_ID=$((ID + 140))
+    echo "Sending to CAN ID $CAN_ID: cansend can0 ${CAN_ID}#${START_CMD}"
+    sudo cansend can0 ${CAN_ID}#${START_CMD}
+    echo "Sending to CAN ID $CAN_ID: cansend can0 ${CAN_ID}#${CLEAR_CMD}"
+    sudo cansend can0 ${CAN_ID}#${CLEAR_CMD}
+    echo "Sending to CAN ID $CAN_ID: cansend can0 ${CAN_ID}#${SPEED_CMD}"
+    sudo cansend can0 ${CAN_ID}#${SPEED_CMD}
 done
 echo "Speed setup commands sent to CAN IDs 21-26."
