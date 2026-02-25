3ba1f795c248259c74e16678ab1eafba102874a6
Jonathan Gibbs <jon@gibbs.tk>
2026-02-25 22:03:55 +0000

    put the clear fault flags command before the enable command

--- speed_cansend.sh
+++ speed_cansend.sh
@@ -16,10 +16,10 @@ echo "CAN interface can0 set up successfully."
 #Enable the motors in speed mode (not torque mode)
 for ID in 21 22 23 24 25 26; do
     CAN_ID=$((ID + 140))
-    echo "Sending to CAN ID $CAN_ID: cansend can0 ${CAN_ID}#${START_CMD}"
-    sudo cansend can0 ${CAN_ID}#${START_CMD}
     echo "Sending to CAN ID $CAN_ID: cansend can0 ${CAN_ID}#${CLEAR_CMD}"
     sudo cansend can0 ${CAN_ID}#${CLEAR_CMD}
+    echo "Sending to CAN ID $CAN_ID: cansend can0 ${CAN_ID}#${START_CMD}"
+    sudo cansend can0 ${CAN_ID}#${START_CMD}
     echo "Sending to CAN ID $CAN_ID: cansend can0 ${CAN_ID}#${SPEED_CMD}"
     sudo cansend can0 ${CAN_ID}#${SPEED_CMD}
 done
