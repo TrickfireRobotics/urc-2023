b945a0c1f56a63e88101781ab3da3f9d41288b14
Jonathan Gibbs <jon@gibbs.tk>
2026-02-27 00:51:06 +0000

    added myactuator dependency in setup.py and packaage.xml

--- src/can_rmdx8/setup.py
+++ src/can_rmdx8/setup.py
@@ -10,7 +10,7 @@ setup(
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=["setuptools"],
+    install_requires=["setuptools", "myactuator_rmd_py"],
     zip_safe=True,
     maintainer="trickfire",
     maintainer_email="trickfire@todo.todo",
