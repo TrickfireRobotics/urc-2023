02055c594a9666fc0c9e5bb732732831ae69cdda
Kaitlin <cookykn@gmail.com>
2026-02-27 23:07:11 +0000

    indiv turntable changes fofr aaaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/arm/arm/individual_control_vel.py
+++ src/arm/arm/individual_control_vel.py
@@ -1,3 +1,6 @@
+import time
+
+import can
 from rclpy.node import Node
 from std_msgs.msg import Float32
 
@@ -14,22 +17,6 @@ class IndividualControlVel:
         self.bot_interface = interface
 
         self.can_send = False
-
-        # Left wrist motor
-        self.left_wrist_motor_sub = ros_node.create_subscription(
-            Float32, "left_wrist_cw", self.leftWristCW, 10
-        )
-        self.left_wrist_motor_sub = ros_node.create_subscription(
-            Float32, "left_wrist_ccw", self.leftWristCCW, 10
-        )
-
-        # Right wrist motor
-        self.left_wrist_motor_sub = ros_node.create_subscription(
-            Float32, "right_wrist_cw", self.rightWristCW, 10
-        )
-        self.left_wrist_motor_sub = ros_node.create_subscription(
-            Float32, "right_wrist_ccw", self.rightWristCCW, 10
-        )
 
         # Elbow motor
         self.elbow_up_sub = ros_node.create_subscription(Float32, "elbow_up", self.elbowUp, 10)
@@ -56,61 +43,106 @@ class IndividualControlVel:
             Float32, "turntable_ccw", self.turntableCCW, 10
         )
 
-    def leftWristCW(self, msg: Float32) -> None:
-        if not self.can_send:
-            return
+        self.turntableMotorPos = 0
+        self.turntableMotorSpeed = 0
+        self.turntableMotorFaultCode = 0
 
-        joystick = msg.data
+        self.updateTurntableMotorState()
 
-        if joystick > 0:
-            self._ros_node.get_logger().info("Left Wrist CW" + str(joystick))
-            self.bot_interface.runMotorSpeed(MotorConfigs.ARM_LEFT_WRIST_MOTOR, self.WRIST_VEL)
+    def updateTurntableMotorState(self, send_msg_before: bool = True) -> None:
+        with can.Bus(interface="socketcan", channel="can0", receive_own_messages=True) as bus:
+            if send_msg_before:
+                msg = can.Message(
+                    arbitration_id=0x00000305,  # velocity loop
+                    data=[0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                    is_extended_id=True,
+                )
+                try:
+                    bus.send(msg)
+                    self._ros_node.get_logger().info(f"Message sent on {bus.channel_info}")
+                except can.CanError:
+                    self._ros_node.get_logger().info("Message NOT sent")
 
+            for msg in bus:
+                self._ros_node.get_logger().info(f"{msg.arbitration_id:X}: {msg.data}")
+                if msg.arbitration_id == 0x00002905:
+                    self.turntableMotorPos = int(
+                        self.uint8_to_bin(msg.data[0]) + self.uint8_to_bin(msg.data[1]), 2
+                    )
+                    self._ros_node.get_logger().info(
+                        f"Turntable motor position: {self.turntableMotorPos}"
+                    )
+                    self.turntableMotorSpeed = int(
+                        self.uint8_to_bin(msg.data[2]) + self.uint8_to_bin(msg.data[3]), 2
+                    )
+                    self._ros_node.get_logger().info(
+                        f"Turntable motor speed {self.turntableMotorSpeed}"
+                    )
+                    self.turntableMotorFaultCode = msg.data[7]
+                    if self.turntableMotorFaultCode != 0:
+                        self._ros_node.get_logger().info(
+                            f"TURNTABLE FAULT: {self.turntableMotorFaultCode}"
+                        )
+
+    def moveTurntableMotorLilBit(self) -> None:
+        self._ros_node.get_logger().info("MOVING TURNTABLE POSITIVE")
+        self.updateTurntableMotorState()
+        with can.Bus(interface="socketcan", channel="can0", receive_own_messages=True) as bus:
+            msg = can.Message(
+                arbitration_id=0x00000305, data=[0x00, 0x00, 0x09, 0xC4], is_extended_id=True
+            )  # help
+            try:
+                bus.send(msg)
+                self._ros_node.get_logger().info(f"Message sent on {bus.channel_info}")
+            except can.CanError:
+                self._ros_node.get_logger().info("Message NOT sent")
+                return
+
+            time.sleep(0.5)
+
+            msg2 = can.Message(
+                arbitration_id=0x00000305, data=[0x00, 0x00, 0x00, 0x00], is_extended_id=True
+            )
+
+            try:
+                bus.send(msg2)
+                self._ros_node.get_logger().info(f"Message sent on {bus.channel_info}")
+            except can.CanError:
+                self._ros_node.get_logger().info("Message NOT sent")
+                return
+
+    def moveTurntableMotorLilBitOtherWay(self) -> None:
+        self._ros_node.get_logger().info("MOVING TURNTABLE NEGATIVE")
+        self.updateTurntableMotorState()
+        with can.Bus(interface="socketcan", channel="can0", receive_own_messages=True) as bus:
+            msg = can.Message(
+                arbitration_id=0x00000305, data=[0x00, 0x00, 0xF6, 0x3C], is_extended_id=True
+            )  # help
+            try:
+                bus.send(msg)
+                self._ros_node.get_logger().info(f"Message sent on {bus.channel_info}")
+            except can.CanError:
+                self._ros_node.get_logger().info("Message NOT sent")
+                return
+
+            time.sleep(0.5)
+
+            msg2 = can.Message(
+                arbitration_id=0x00000305, data=[0x00, 0x00, 0x00, 0x00], is_extended_id=True
+            )
+
+            try:
+                bus.send(msg2)
+                self._ros_node.get_logger().info(f"Message sent on {bus.channel_info}")
+            except can.CanError:
+                self._ros_node.get_logger().info("Message NOT sent")
+                return
+
+    def uint8_to_bin(self, value: int) -> str:
+        if 0 <= value <= 255:  # Ensure it's within uint8 range
+            return format(value, "08b")  # Format as an 8-bit binary string
         else:
-            self._ros_node.get_logger().info("Left Wrist STOP")
-            self.bot_interface.stopMotor(MotorConfigs.ARM_LEFT_WRIST_MOTOR)
-
-    def leftWristCCW(self, msg: Float32) -> None:
-        if not self.can_send:
-            return
-
-        joystick = msg.data
-
-        if joystick > 0:
-            self._ros_node.get_logger().info("Left Wrist CCW" + str(joystick))
-            self.bot_interface.runMotorSpeed(MotorConfigs.ARM_LEFT_WRIST_MOTOR, -self.WRIST_VEL)
-
-        else:
-            self._ros_node.get_logger().info("Left Wrist STOP")
-            self.bot_interface.stopMotor(MotorConfigs.ARM_LEFT_WRIST_MOTOR)
-
-    def rightWristCW(self, msg: Float32) -> None:
-        if not self.can_send:
-            return
-
-        joystick = msg.data
-
-        if joystick > 0:
-            self._ros_node.get_logger().info("Right Wrist CW" + str(joystick))
-            self.bot_interface.runMotorSpeed(MotorConfigs.ARM_RIGHT_WRIST_MOTOR, -self.WRIST_VEL)
-
-        else:
-            self._ros_node.get_logger().info("Right Wrist STOP")
-            self.bot_interface.stopMotor(MotorConfigs.ARM_RIGHT_WRIST_MOTOR)
-
-    def rightWristCCW(self, msg: Float32) -> None:
-        if not self.can_send:
-            return
-
-        joystick = msg.data
-
-        if joystick > 0:
-            self._ros_node.get_logger().info("Right Wrist CCW" + str(joystick))
-            self.bot_interface.runMotorSpeed(MotorConfigs.ARM_RIGHT_WRIST_MOTOR, -self.WRIST_VEL)
-
-        else:
-            self._ros_node.get_logger().info("Right Wrist STOP")
-            self.bot_interface.stopMotor(MotorConfigs.ARM_RIGHT_WRIST_MOTOR)
+            raise ValueError("Value must be between 0 and 255")
 
     def elbowUp(self, msg: Float32) -> None:
         if not self.can_send:
@@ -175,12 +207,11 @@ class IndividualControlVel:
         data = msg.data
 
         if data > 0:
-            self._ros_node.get_logger().info("Turntable clock wise")
-            self.bot_interface.runMotorSpeed(MotorConfigs.ARM_TURNTABLE_MOTOR, self.VEL)
+            self.moveTurntableMotorLilBit()
 
         else:
             self._ros_node.get_logger().info("Turntable STOP")
-            self.bot_interface.stopMotor(MotorConfigs.ARM_TURNTABLE_MOTOR)
+            self.updateTurntableMotorState()
 
     def turntableCCW(self, msg: Float32) -> None:
         if not self.can_send:
@@ -189,9 +220,8 @@ class IndividualControlVel:
         data = msg.data
 
         if data > 0:
-            self._ros_node.get_logger().info("Turntable counter clock wise")
-            self.bot_interface.runMotorSpeed(MotorConfigs.ARM_TURNTABLE_MOTOR, -self.VEL)
+            self.moveTurntableMotorLilBitOtherWay()
 
         else:
             self._ros_node.get_logger().info("Turntable STOP")
-            self.bot_interface.stopMotor(MotorConfigs.ARM_TURNTABLE_MOTOR)
+            self.updateTurntableMotorState()
