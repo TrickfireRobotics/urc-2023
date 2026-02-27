e4bb1475fb7d7a1992f599623e2c1dbf0b71545a
Kaitlin <cookykn@gmail.com>
2026-02-27 19:31:33 +0000

    added state checking method for turntable motor

--- src/arm/arm/inverse_kinematics.py
+++ src/arm/arm/inverse_kinematics.py
@@ -120,6 +120,53 @@ class InverseKinematics:
         self._target_axes = sg.Axes(0.1, pose=self.target)
         threading.Thread(target=self._startSwift).start()
 
+        self.turntableMotorPos = 0
+        self.turntableMotorSpeed = 0
+        self.turntableMotorFaultCode = 0
+
+        with can.Bus(interface="socketcan", channel="can0", receive_own_messages=True) as bus:
+            # send zero velocity
+            msg = can.Message(
+                arbitration_id=0x00000305,  # velocity loop
+                data=[0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                is_extended_id=True,
+            )
+            try:
+                bus.send(msg)
+                self._ros_node.get_logger().info(f"Message sent on {bus.channel_info}")
+            except can.CanError:
+                self._ros_node.get_logger().info("Message NOT sent")
+        self.updateTurntableMotorState()
+
+    def updateTurntableMotorState(self) -> None:
+        with can.Bus(interface="socketcan", channel="can0", receive_own_messages=True) as bus:
+            for msg in bus:
+                self._ros_node.get_logger().info(f"{msg.arbitration_id:X}: {msg.data}")
+                if msg.arbitration_id == 0x00002905:
+                    self.turntableMotorPos = int(
+                        self.uint8_to_bin(msg.data[0]) + self.uint8_to_bin(msg.data[1]), 2
+                    )
+                    self._ros_node.get_logger().info(
+                        f"Turntable motor position: {self.turntableMotorPos}"
+                    )
+                    self.turntableMotorSpeed = int(
+                        self.uint8_to_bin(msg.data[2]) + self.uint8_to_bin(msg.data[3]), 2
+                    )
+                    self._ros_node.get_logger().info(
+                        f"Turntable motor speed {self.turntableMotorSpeed}"
+                    )
+                    self.turntableMotorFaultCode = msg.data[7]
+                    if self.turntableMotorFaultCode != 0:
+                        self._ros_node.get_logger().info(
+                            f"TURNTABLE FAULT: {self.turntableMotorFaultCode}"
+                        )
+
+    def uint8_to_bin(self, value: int) -> str:
+        if 0 <= value <= 255:  # Ensure it's within uint8 range
+            return format(value, "08b")  # Format as an 8-bit binary string
+        else:
+            raise ValueError("Value must be between 0 and 255")
+
     def _startSwift(self) -> None:
         try:
             self._env.launch(realtime=True, comms="websocket")
