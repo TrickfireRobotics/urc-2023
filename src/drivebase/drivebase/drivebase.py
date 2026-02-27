df708467c3e615803754e020a581f37222c46864
Kaitlin <cookykn@gmail.com>
2026-02-27 03:58:24 +0000

    add stop to all motors when it hasn't received msg from mission control

--- src/drivebase/drivebase/drivebase.py
+++ src/drivebase/drivebase/drivebase.py
@@ -1,4 +1,5 @@
 import sys
+import time
 
 import rclpy
 from rclpy.executors import ExternalShutdownException
@@ -25,6 +26,13 @@ class Drivebase(Node):
             Float32, "move_right_drivebase_side_message", self.moveRightSide, 10
         )
 
+        self.create_timer(0.05, self.stopMotors)
+        self._last_received: list[float] = []
+
+    def appendToLastReceived(self) -> None:
+        last_receieved_index = len(self._last_received) - 1
+        self._last_received.append(time.time())
+
     def moveLeftSide(self, msg: Float32) -> None:
         vel = msg.data * self.SPEED
         self.bot_interface.runMotorSpeed(MotorConfigs.FRONT_LEFT_DRIVE_MOTOR, -vel)
@@ -44,6 +52,20 @@ class Drivebase(Node):
     def turnRight(self, msg: Float32) -> None:
         self.moveLeftSide(-msg)
         self.moveRightSide(msg)
+
+    def stopMotors(self) -> None:
+        last_receieved_index = len(self._last_received) - 1
+        time_delta = time.time() - self._last_received[last_receieved_index]
+        if time_delta > 0.25:
+
+            self.get_logger().info("STOPPING ALL MOTORS")
+            self.bot_interface.stopMotor(MotorConfigs.FRONT_LEFT_DRIVE_MOTOR)
+            self.bot_interface.stopMotor(MotorConfigs.MID_LEFT_DRIVE_MOTOR)
+            self.bot_interface.stopMotor(MotorConfigs.REAR_LEFT_DRIVE_MOTOR)
+
+            self.bot_interface.stopMotor(MotorConfigs.FRONT_RIGHT_DRIVE_MOTOR)
+            self.bot_interface.stopMotor(MotorConfigs.MID_RIGHT_DRIVE_MOTOR)
+            self.bot_interface.stopMotor(MotorConfigs.REAR_RIGHT_DRIVE_MOTOR)
 
 
 def main(args: list[str] | None = None) -> None:
