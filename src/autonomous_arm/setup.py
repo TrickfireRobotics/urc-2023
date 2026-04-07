5a1c143b7055a79b920de771df3fd52d98ca628d
alexrsing <alex@sing.email>
2026-04-07 22:57:12 +0000

    [autonomous][arm-typing] Creates separate package for autonomous arm files rather than sitting in arm package

---
+++ src/autonomous_arm/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'autonomous_arm'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='trickfire',
+    maintainer_email='alex@sing.email',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
