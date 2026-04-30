456cba84a2f047b0a1a06d078dfc52b8a28ba9d4
Kaitlin <cookykn@gmail.com>
2026-04-30 23:48:51 +0000

    changed motor config

--- src/lib/configs.py
+++ src/lib/configs.py
@@ -126,13 +126,8 @@ class MotorConfigs:
     FRONT_LEFT_DRIVE_MOTOR = RMDx8MotorConfig(can_id=22)
 
     # Arm
-    ARM_SHOULDER_MOTOR = MoteusMotorConfig(
+    ARM_SHOULDER_MOTOR = RMDx8MotorConfig(
         can_id=1,
-        config={
-            "servo.max_voltage": 56.0,
-            "servopos.position_min": math.nan,
-            "servopos.position_max": math.nan,
-        },
     )
     ARM_ELBOW_MOTOR = MoteusMotorConfig(
         can_id=2,
